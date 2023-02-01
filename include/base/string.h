d8b1f49f55d4d7b3b27ea01e07a8dee182aae238
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 08:02:14 -0500

    Add string.c. Add string constructor.

---
+++ include/base/string.h
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_BASE_STRING_H
+#define LILY_BASE_STRING_H
+
+#include <base/types.h>
+
+typedef struct String {
+	char *buffer;
+	Usize len;
+	Usize capacity;
+	Usize default_capacity;
+} String;
+
+/**
+ *
+ * @brief Construct String type.
+ */
+String* __new__String();
+
+/**
+ *
+ * @brief Construct String type with default buffer items.
+ */
+String* from__String(char *buffer);
+
+/**
+ *
+ * @brief Free String buffer.
+ */
+void __free__String(String *self);
+
+#endif // LILY_BASE_STRING_H
---
+++ src/base/string.c
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/string.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+String* __new__String() {
+	String *s = malloc(sizeof(String));
+
+	s->buffer = malloc(1);
+	s->buffer[0] = '\0';
+	s->len = 0;
+	s->capacity = 0;
+	s->default_capacity = 4;
+
+	return s;
+}
+
+String* from__String(char *buffer) {
+	String *s = malloc(sizeof(String));
+
+	s->capacity = strlen(buffer) * 2;
+	s->default_capacity = s->capacity;
+	s->buffer = malloc(s->capacity);
+	s->len = strlen(buffer);	
+
+	for (int i = 0; i < s->len; i++) {
+		s->buffer[i] = buffer[i];
+	}
+
+	s->buffer[s->len] = '\0';
+
+	return s;
+}
+
+void __free__String(String *self) {
+	free(self->buffer);
+	free(self);
+}
