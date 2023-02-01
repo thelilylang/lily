7da368e3bfc74cdeb91a25f161762ee36fb3ab52
ArthurPV <arthurpontiervalois@gmail.com>
2023-01-31 21:51:28 -0500

    Add color.h and color.c

---
+++ include/base/color.h
@@ -0,0 +1,84 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_BASE_COLOR_H
+#define LILY_BASE_COLOR_H
+
+#include <base/types.h>
+
+#define RED(s) red__Color(s)
+#define GREEN(s) green__Color(s)
+#define YELLOW(s) yellow__Color(s)
+#define BLUE(s) blue__Color(s)
+#define MAGENTA(s) magenta__Color(s)
+#define CYAN(s) cyan__Color(s)
+#define BOLD(s) bold__Style(s)
+
+/**
+ *
+ * @return the formatted char* in red color.
+ */
+char*
+red__Color(char* s);
+
+/**
+ *
+ * @return the formatted char* in green color.
+ */
+char*
+green__Color(char* s);
+
+/**
+ *
+ * @return the formatted char* in yellow color.
+ */
+char*
+yellow__Color(char* s);
+
+/**
+ * @return the formatted char* in blue color.
+ */
+char*
+blue__Color(char* s);
+
+/**
+ *
+ * @return the formatted char* in magenta color.
+ */
+char*
+magenta__Color(char* s);
+
+/**
+ * @return the formatted char* in cyan color.
+ */
+char*
+cyan__Color(char* s);
+
+/**
+ * @return the formatted char* in bold style.
+ */
+char*
+bold__Style(char* s);
+
+#endif // LILY_BASE_COLOR_H
---
+++ src/base/color.c
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/color.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char*
+red__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[31m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[31m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+green__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[32m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[32m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+yellow__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[33m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[33m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+blue__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[34m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[34m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+magenta__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[35m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[35m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+cyan__Color(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[36m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[36m%s\x1b[0m", s);
+    return res;
+}
+
+char*
+bold__Style(char* s)
+{
+    size_t size = snprintf(NULL, 0, "\x1b[1m%s\x1b[0m", s) + 1;
+    char *res = malloc(size);
+    snprintf(res, size, "\x1b[1m%s\x1b[0m", s);
+    return res;
+}
