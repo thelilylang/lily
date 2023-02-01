19cbc5a8bb0385b69edc6836d66dd74451e6254c
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 07:29:35 -0500

    Add file.c and file.h. Add is_directory and get_extension function.

---
+++ src/base/file.c
@@ -0,0 +1,77 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/file.h>
+#include <base/platform.h>
+#include <base/macros.h>
+#include <base/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+#if defined(LILY_WINDOWS_OS)
+#include <windows.h>
+#endif
+
+bool is_directory__Path(const char *path) {
+#if defined(LILY_WINDOWS_OS)
+	DWORD dwAttrib = GetFileAttributes(path);
+
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+#else
+	DIR *dir = opendir(path);
+
+	if (ENOENT == errno || dir == NULL) {
+		return false;
+	}
+
+	closedir(dir);
+#endif
+
+	return true;
+}
+
+char* get_extension__Path(const char *path) {
+	bool extension_started = false;
+	Usize size = 1;
+	char *extension = malloc(1);
+	Usize len = strlen(path);
+
+	for (Usize i = 0; i < len; i++) {
+		if (path[i] == '.' && path[i + 1 < len ? i + 1 : i] != '.' && path[i + 1 < len ? i + 1 : i] != '/' && path[i + 1 < len ? i + 1 : i] != '\\') {
+			extension_started = true;
+		}
+
+		if (extension_started) {
+			extension = realloc(extension, size + 1);
+			extension[size - 1] = path[i];
+			extension[size++] = '\0';
+		}
+	}
+
+	return extension;
+}
