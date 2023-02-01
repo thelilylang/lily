b00dc5c57e5483d3bc2e348729edff8c96190d49
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 13:15:29 -0500

    Add some comments

--- tests/base/base.c
+++ tests/base/base.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 int main() {
+	// Test Vec
 	test_new__Vec();
 	test_from__Vec();
 	test_get__Vec();
@@ -13,11 +14,13 @@ int main() {
 	test_remove__Vec();
 	test_reverse__Vec();
 
+	// Test String
 	test_new__String();
 	test_from__String();
 	test_pop__String();
 	test_push__String();
 
+	// Test HashMap
 	test_new__HashMap();
 
 	puts("\x1b[32mAll tests pass\x1b[0m");
