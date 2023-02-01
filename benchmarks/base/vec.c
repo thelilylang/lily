00cde36f16f68862433af4a94c76f9bb7e49300e
ArthurPV <arthurpontiervalois@gmail.com>
2023-01-31 21:40:06 -0500

    Update bench

--- benchmarks/base/vec.c
+++ benchmarks/base/vec.c
@@ -20,4 +20,23 @@ int main() {
 
 		printf("Time: %.2fs\n", (double)(clock() - start) / CLOCKS_PER_SEC);
 	}
+
+	printf("Bench: push 1'000'000'000 items and remove 1'000'000'000\n");
+
+	{
+		clock_t start = clock();
+		Vec *v = NEW(Vec);
+
+		for (Usize i = 0; i < 1'000'000'000; i++) {
+			push__Vec(v, (Usize*)i);
+		}
+
+		for (Usize i = 0; i < 1'000'000'000; i++) {
+			pop__Vec(v);
+		}
+
+		FREE(Vec, v);
+
+		printf("Time: %.2fs\n", (double)(clock() - start) / CLOCKS_PER_SEC);
+	}
 }
