b8aa8fae8721856ced97cb6f3441046b19d36370
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 12:48:10 -0500

    Add test_push__String

--- tests/base/string.c
+++ tests/base/string.c
@@ -31,3 +31,24 @@ void test_from__String() {
 
 	FREE(String, s);
 }
+
+void test_push__String() {
+	String *s = NEW(String);
+
+	push__String(s, 'H');
+	push__String(s, 'e');
+	push__String(s, 'l');
+	push__String(s, 'l');
+	push__String(s, 'o');
+
+	ASSERT(s->len == 5);
+	ASSERT(s->capacity == 8);
+	ASSERT(s->default_capacity == 4);
+
+	ASSERT(s->buffer[0] == 'H');
+	ASSERT(s->buffer[1] == 'e');
+	ASSERT(s->buffer[2] == 'l');
+	ASSERT(s->buffer[3] == 'l');
+	ASSERT(s->buffer[4] == 'o');
+	ASSERT(s->buffer[5] == '\0');
+}
