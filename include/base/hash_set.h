55f1a16c9d107dbb9bdedbff095aefb7dad2e1c5
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 23:13:11 -0500

    Add hash_set.h and hash_set.c

---
+++ include/base/hash_set.h
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_BASE_HASH_SET_H
+#define LILY_BASE_HASH_SET_H
+
+#include <base/types.h>
+#include <base/vec.h>
+
+#include <stdbool.h>
+
+typedef struct HashSet
+{
+  Vec *values;
+  Usize len;
+} HashSet;
+
+/**
+ *
+ * @brief Construct HashSet type.
+ */
+HashSet *
+__new__HashSet();
+
+/**
+ *
+ * @brief Get value from values.
+ * @return If the value does not exist, return NULL.
+ */
+void *
+get__HashSet(HashSet *self, void *value);
+
+/**
+ *
+ * @brief Insert value into HashSet.
+ * @return If the value already exists, return false otherwise return true.
+ */
+bool
+insert__HashSet(HashSet *self, void *value);
+
+#endif // LILY_BASE_HASH_SET_H
---
+++ src/base/hash_set.c
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/hash_set.h>
+#include <base/new.h>
+
+#include <stdlib.h>
+
+HashSet *
+__new__HashSet()
+{
+  HashSet *hs = malloc(sizeof(HashSet));
+
+  hs->values = NEW(Vec);
+  hs->len = 0;
+
+  return hs;
+}
+
+void *
+get__HashSet(HashSet *self, void *value)
+{
+  for (Usize i = 0; i < self->len; i++) {
+    if (self->values->buffer[i] == value) {
+      return self->values->buffer[i];
+    }
+  }
+
+  return NULL;
+}
+
+bool
+insert__HashSet(HashSet *self, void *value)
+{
+  for (Usize i = 0; i < self->len; i++) {
+    if (self->values->buffer[i] == value) {
+      return false;
+    }
+  }
+
+  push__Vec(self->values, value);
+  self->len++;
+
+  return true;
+}
