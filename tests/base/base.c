54ca873acb8e1af25e8715cc7f22f82d9abc9fdb
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-01 23:38:57 -0500

    Change test order

--- tests/base/base.c
+++ tests/base/base.c
@@ -11,20 +11,12 @@
 int
 main()
 {
-    // Test Vec
-    test_new__Vec();
-    test_from__Vec();
-    test_get__Vec();
-    test_pop__Vec();
-    test_push__Vec();
-    test_remove__Vec();
-    test_reverse__Vec();
+    // Test atoi
+    test_atoi();
 
-    // Test String
-    test_new__String();
-    test_from__String();
-    test_pop__String();
-    test_push__String();
+    // Test format
+    test_format_s_specifier();
+    test_format_sa_specifier();
 
     // Test HashMap
     test_new__HashMap();
@@ -34,18 +26,26 @@ main()
     // Test HashSet
     test_new__HashSet();
 
-    // Test format
-    test_format_s_specifier();
-    test_format_sa_specifier();
-
-    // Test atoi
-    test_atoi();
-
     // Test itoa
     test_itoa_base_10();
     test_itoa_base_2();
     test_itoa_base_8();
     test_itoa_base_16();
+
+    // Test String
+    test_new__String();
+    test_from__String();
+    test_pop__String();
+    test_push__String();
+
+    // Test Vec
+    test_new__Vec();
+    test_from__Vec();
+    test_get__Vec();
+    test_pop__Vec();
+    test_push__Vec();
+    test_remove__Vec();
+    test_reverse__Vec();
 
     puts("\x1b[32mAll tests pass\x1b[0m");
 }
