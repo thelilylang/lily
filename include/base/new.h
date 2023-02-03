ebb70e6c2fd3d816cc268839757fa956fe5ff973
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-03 03:00:23 -0500

    Update new.h

--- include/base/new.h
+++ include/base/new.h
@@ -28,6 +28,7 @@
 #define NEW(name, ...) __new__##name(__VA_ARGS__)
 #define NEW_VARIANT(name, variant, ...) __new__##name##__##variant(__VA_ARGS__)
 #define FREE(name, ...) __free__##name(__VA_ARGS__)
-#define FREE_VARIANT(name, variant, ...) __free__##name##__##variant(__VA_ARGS__)
+#define FREE_VARIANT(name, variant, ...) \
+    __free__##name##__##variant(__VA_ARGS__)
 
 #endif // LILY_BASE_NEW_H
