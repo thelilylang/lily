9792fd805f050e970d07515bbb8b4431b5fb2d1c
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-03 11:28:55 -0500

    Update config.h

--- include/cli/config.h
+++ include/cli/config.h
@@ -35,4 +35,115 @@
 #include <cli/config/test.h>
 #include <cli/config/to.h>
 
+enum ConfigKind
+{
+    CONFIG_KIND_BUILD,
+    CONFIG_KIND_CC,
+    CONFIG_KIND_COMPILE,
+    CONFIG_KIND_CPP,
+    CONFIG_KIND_INIT,
+    CONFIG_KIND_NEW,
+    CONFIG_KIND_RUN,
+    CONFIG_KIND_TEST,
+    CONFIG_KIND_TO
+};
+
+typedef struct Config
+{
+    enum ConfigKind kind;
+    union
+    {
+        BuildConfig build;
+        CcConfig cc;
+        CompileConfig compile;
+        CppConfig cpp;
+        InitConfig init;
+        NewConfig new;
+        RunConfig run;
+        TestConfig test;
+        ToConfig to;
+    };
+} Config;
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_BUILD).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, build, BuildConfig build)
+{
+    return (Config){ .kind = CONFIG_KIND_BUILD, .build = build };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_CC).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, cc, CcConfig cc)
+{
+    return (Config){ .kind = CONFIG_KIND_CC, .cc = cc };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_COMPILE).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, compile, CompileConfig compile)
+{
+    return (Config){ .kind = CONFIG_KIND_COMPILE, .compile = compile };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_CPP).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, cpp, CppConfig cpp)
+{
+    return (Config){ .kind = CONFIG_KIND_CPP, .cpp = cpp };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_INIT).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, init, InitConfig init)
+{
+    return (Config){ .kind = CONFIG_KIND_INIT, .init = init };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_NEW).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, new, NewConfig new)
+{
+    return (Config){ .kind = CONFIG_KIND_NEW, .new = new };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_RUN).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, run, RunConfig run)
+{
+    return (Config){ .kind = CONFIG_KIND_RUN, .run = run };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_TEST).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, test, TestConfig test)
+{
+    return (Config){ .kind = CONFIG_KIND_TEST, .test = test };
+}
+
+/**
+ *
+ * @brief Construct Option type (CONFIG_KIND_TO).
+ */
+inline VARIANT_CONSTRUCTOR(Config, Config, to, ToConfig to)
+{
+    return (Config){ .kind = CONFIG_KIND_TO, .to = to };
+}
+
 #endif // LILY_CLI_CONFIG_H
