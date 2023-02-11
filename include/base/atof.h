0a5599d99406910beba05e9000f2c7cf283cb55d
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-10 23:34:17 -0500

    Update atof.h

--- include/base/atof.h
+++ include/base/atof.h
@@ -27,6 +27,35 @@
 
 #include <base/types.h>
 
+#define FLOAT_SCIENTIFIC 0
+#define FLOAT_NORMAL 1
+
+// MAX: 340282346999999984391321947108527833088, 3.40282347E+38
+// MIN: -340282346999999984391321947108527833088, -3.40282347E+38
+// NOTE: return 0 if is overflow/underflow otherwise return 1.
+// NOTE: this check not support lead by 0 will do nothing (e.g. 000000033)
+// NOTE: not currently support 3e3 syntax (only syntax with sign)
+#define CHECK_FLOAT32_SCIENTIFIC_OVERFLOW_FROM_STRING(x, len) \
+    ((x[0] != '-' && (x[0] < '3' ? 1 : x[0] > '3' ? (x[len - 1] > '8' && (x[len - 2] == '-' || x[len - 2] == '+') ? 1 : x[len - 1] > '8' ? 0 : x[len - 1] < '8' && (x[len - 2] == '-' || x[len - 2] == '+') ? 1 : x[len - 2] > '3' ? 0 : x[len - 2] <= '3' && (x[len - 3] == '-' || x[len - 3] == '+') ? 1 : 0) : x[0] == '3' ? x[len - 1] > '8' && (x[len - 2] == '-' || x[len - 2] == '+') ? 1 : x[len - 1] > '8' ? 0 : x[len - 1] < '8' && (x[len - 2] == '-' || x[len - 2] == '+') ? 1 : x[len - 2] > '3' ? 0 : x[len - 2] <= '3' && (x[len - 3] == '-' || x[len - 3] == '+') ? 1 : 0)))
+
+#define CHECK_FLOAT32_NORMAL_OVERFLOW_FROM_STRING(x, len) 0
+#define CHECK_FLOAT32_OVERFLOW_FROM_STRING(x, len, float_kind) 0
+
+// MAX:
+// 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368,
+// 1.7976931348623157E+308
+// MIN:
+// -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368,
+// -1.7976931348623157E+308
+// NOTE: return 0 if is overflow/underflow otherwise
+// return 1.
+// NOTE: this check not support lead by 0 will do nothing (e.g.
+// 000000033)
+// NOTE: not currently support 3e3 syntax (only syntax with sign)
+#define CHECK_FLOAT64_SCIENTIFIC_OVERFLOW_FROM_STRING(x) 0
+#define CHECK_FLOAT64_NORMAL_OVERFLOW_FROM_STRING(x) 0
+#define CHECK_FLOAT64_OVERFLOW_FROM_STRING(x, float_kind) 0
+
 /**
  *
  * @brief Convert a char* to Float32.
