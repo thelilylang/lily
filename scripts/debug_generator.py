05a282ff80745cde575e33a757a74feedf13fe82
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-12 00:12:02 -0500

    Update debug_generator.py

--- scripts/debug_generator.py
+++ scripts/debug_generator.py
@@ -15,7 +15,7 @@ class EnumDebugGenerator:
     variants : str
 
     def __parse_variants(self) -> list[str]:
-        return list(map(lambda x: re.sub(r"\s+", '', x), list(filter(lambda x: x != ' ', self.variants.split(",")))))
+        return list(map(lambda x: re.sub(r"\s+", "", x), list(filter(lambda x: x != ' ', self.variants.split(",")))))
 
     def generate_body(self) -> str:
         parsed_variants = self.__parse_variants()
@@ -34,6 +34,39 @@ class EnumDebugGenerator:
 @dataclass
 class StructDebugGenerator:
     fields : str
+    struct_name : str
+
+    def __parse_fields(self) -> list[{"name": str, "type": str}]:
+        res = []
+
+        for field in self.fields.split(";"):
+            field = field.split(' ')
+            name = re.sub(r"\s+", "", field[1])
+            dt = re.sub(r"\s+", "", field[0])
+            re.sub(r"[^a-zA-Z][0-9]", "", dt)
+            res.append({"name": name, "type": dt})
+
+        return res
+
+    def generate_body(self) -> str:
+        parsed_fields = self.__parse_fields()
+        res = f"return format(\"{self.struct_name}{{{{\n"
+
+        for p_field in parsed_fields:
+            name = p_field["name"]
+            res += f"{name} = "
+            res += "{Sr},\\n"
+
+        res += "}\", "
+
+        for p_field in parsed_fields:
+            name = p_field["name"]
+            dt = p_field["type"]
+            res += f"to_string__Debug__{dt}(self->{name})),"
+
+        res = res[:-1] + ");"
+
+        return res
 
 def read_file(filename : str) -> str:
     with open(filename, "r") as f:
@@ -48,8 +81,11 @@ if __name__ == "__main__":
         enum_dbg_generator = EnumDebugGenerator(variants)
         print(enum_dbg_generator.generate_body())
     elif answer_t == "struct":
-        fields = input("Please enter the name of the file that contains all the fields you want to generate ")
-        struct_dbg_generator = StructDebugGenerator(fields)
+        struct_name = input("Please enter the name of the struct ")
+        filename = input("Please enter the name of the file that contains all the fields you want to generate ")
+        fields = read_file(filename)
+        struct_dbg_generator = StructDebugGenerator(fields, struct_name)
+        print(struct_dbg_generator.generate_body())
     else:
         print("unknown kind of type")
         exit(1)
