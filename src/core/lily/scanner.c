eda9665e202c0296d7bd9f6f3c1a12491567e151
ArthurPV <arthurpontiervalois@gmail.com>
2023-02-16 10:17:59 -0500

    Beautify scanner.c

--- src/core/lily/scanner.c
+++ src/core/lily/scanner.c
@@ -1984,7 +1984,9 @@ get_token__Scanner(Scanner *self)
         default: {
             Location location_error = clone__Location(&self->location);
 
-            end__Location(&location_error, self->source.cursor.line, self->source.cursor.column);
+            end__Location(&location_error,
+                          self->source.cursor.line,
+                          self->source.cursor.column);
 
             emit__Diagnostic(
               NEW_VARIANT(
@@ -1993,7 +1995,9 @@ get_token__Scanner(Scanner *self)
                 self->source.file,
                 &location_error,
                 NEW(LilyError, LILY_ERROR_KIND_UNEXPECTED_CHARACTER),
-                init__Vec(1, format__String("remove this character `{c}`", self->source.cursor.current)),
+                init__Vec(1,
+                          format__String("remove this character `{c}`",
+                                         self->source.cursor.current)),
                 NULL,
                 NULL),
               &self->count_error);
