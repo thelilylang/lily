ef7f08cc40d12f2e0f82bf752979acb8d622c194
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-16 14:50:47 -0400

    docs: Add a comment to explain each of the macros contained in `lily.h`.

--- include/core/lily/lily.h
+++ include/core/lily/lily.h
@@ -27,45 +27,53 @@
 
 #include <base/macros.h>
 
+// This is a macro used to display the scanner in debug mode.
 #define DEBUG_SCANNER
 
 #ifndef ENV_DEBUG
 #undef DEBUG_SCANNER
 #endif
 
+// This is a macro used to display the preparser in debug mode.
 #define DEBUG_PREPARSER
 
 #ifndef ENV_DEBUG
 #undef DEBUG_PREPARSER
 #endif
 
+// This is a macro used to display the precompiler in debug mode.
 #define DEBUG_PRECOMPILE
 
 #ifndef ENV_DEBUG
 #undef DEBUG_PRECOMPILE
 #endif
 
+// This is a macro used to display the parser in debug mode.
 #define DEBUG_PARSER
 
 #ifndef ENV_DEBUG
 #undef DEBUG_PARSER
 #endif
 
+// This is a macro used to display the analysis in debug mode.
 #define DEBUG_ANALYSIS
 
 #ifndef ENV_DEBUG
 #undef DEBUG_ANALYSIS
 #endif
 
+// This is a macro used to display the IR in debug mode.
 #define DEBUG_IR
 
 #ifndef ENV_DEBUG
 #undef DEBUG_IR
 #endif
 
+// This is a macro used to stop the compiler process after the preparser.
 #define RUN_UNTIL_PREPARSER
 #undef RUN_UNTIL_PREPARSER
 
+// This is a macro used to stop the compiler process after the precompiler.
 #define RUN_UNTIL_PRECOMPILE
 #undef RUN_UNTIL_PRECOMPILE
 
