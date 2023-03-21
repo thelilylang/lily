4df70e0dfed7caaa38f905512c3b257a4700eac8
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-21 10:24:58 -0400

    Update package.h.

--- include/core/lily/package.h
+++ include/core/lily/package.h
@@ -1,6 +1,8 @@
 #ifndef LILY_CORE_LILY_PACKAGE_H
 #define LILY_CORE_LILY_PACKAGE_H
 
+#include <core/lily/package/default_path.h>
+#include <core/lily/package/dependency_tree.h>
 #include <core/lily/package/library.h>
 #include <core/lily/package/package.h>
 
