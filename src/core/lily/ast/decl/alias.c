36b3c85663063585fa65094bc6f769c4346b0877
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-23 11:32:13 -0400

    Add LilyAstDeclAlias type. Add constructor, debug and destructor for this type.

---
+++ src/core/lily/ast/decl/alias.c
@@ -0,0 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <core/lily/ast/decl/alias.h>
+
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstDeclAlias, const LilyAstDeclAlias *self)
+{
+    return format__String(
+      "LilyAstDeclAlias{{ name = {S}, data_type = {Sr}, visibility = {s} }",
+      self->name,
+      to_string__Debug__LilyAstDataType(self->data_type),
+      to_string__Debug__LilyVisibility(self->visibility));
+}
+#endif
+
+DESTRUCTOR(LilyAstDeclAlias, const LilyAstDeclAlias *self)
+{
+    FREE_MOVE(self->name, FREE(String, self->name));
+    FREE(LilyAstDataType, self->data_type);
+}
