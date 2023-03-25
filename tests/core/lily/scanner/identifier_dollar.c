d22ba913f79267526f5e6fa011388d8d5666e1c6
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-25 15:53:59 -0400

    Add simple for identifier_dollar.

---
+++ tests/core/lily/scanner/identifier_dollar.c
@@ -0,0 +1,25 @@
+#include "util.c"
+
+#include <base/test.h>
+
+#include <stdio.h>
+#include <string.h>
+
+SIMPLE(identifier_dollar, {
+    RUN_SCANNER(FILE_IDENTIFIER_DOLLAR);
+    SCANNER_ITERATOR();
+
+    TEST_ASSERT_EQ(CURRENT()->kind, LILY_TOKEN_KIND_IDENTIFIER_DOLLAR);
+    TEST_ASSERT(!strcmp(NEXT()->identifier_normal->buffer, "Id33"));
+
+    TEST_ASSERT_EQ(CURRENT()->kind, LILY_TOKEN_KIND_IDENTIFIER_DOLLAR);
+    TEST_ASSERT(!strcmp(NEXT()->identifier_normal->buffer, "ffdle"));
+
+    TEST_ASSERT_EQ(CURRENT()->kind, LILY_TOKEN_KIND_IDENTIFIER_DOLLAR);
+    TEST_ASSERT(!strcmp(NEXT()->identifier_normal->buffer, "Iddldl"));
+
+    TEST_ASSERT_EQ(CURRENT()->kind, LILY_TOKEN_KIND_IDENTIFIER_DOLLAR);
+    TEST_ASSERT(!strcmp(NEXT()->identifier_normal->buffer, "_30dd"));
+
+    FREE_SCANNER();
+});
