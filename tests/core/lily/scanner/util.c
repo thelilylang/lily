52b0634bc472dc9dca759b1ae135fb95d3f1a189
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-25 02:51:39 -0400

    Add `util.c`.

---
+++ tests/core/lily/scanner/util.c
@@ -0,0 +1,29 @@
+#include <base/file.h>
+
+#include <core/lily/scanner.h>
+
+LilyScanner
+run_scanner(File *file)
+{
+    LilyScanner scanner =
+      NEW(LilyScanner, NEW(Source, NEW(Cursor, file->content), file));
+
+    run__LilyScanner(&scanner, false);
+
+    return scanner;
+}
+
+#define RUN_SCANNER(filename)                  \
+    char *content = read_file__File(filename); \
+    File file = NEW(File, filename, content);  \
+    LilyScanner scanner = run_scanner(&file);
+
+#define FREE_SCANNER() \
+  FREE(File, &file); \
+  FREE(LilyScanner, &scanner);
+
+#define GET_TOKEN(idx) CAST(LilyToken*, get__Vec(scanner.tokens, idx))
+
+#define SCANNER_ITERATOR() Usize it = 0;
+
+#define NEXT() CAST(LilyToken*, get__Vec(scanner.tokens, it++))
