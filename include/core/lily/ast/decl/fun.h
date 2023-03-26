74741a71e1c5f5b742066c9b82cb5773e4568034
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-26 17:06:58 -0400

    Add LilyAstDeclFun type. Add constructor, debug and destructor for this type.

--- include/core/lily/ast/decl/fun.h
+++ include/core/lily/ast/decl/fun.h
@@ -1,28 +1,170 @@
 /*
-* MIT License
-*
-* Copyright (c) 2022-2023 ArthurPV
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef LILY_CORE_LILY_AST_DECL_FUN_H
 #define LILY_CORE_LILY_AST_DECL_FUN_H
+
+#include <base/string.h>
+
+#include <core/lily/ast/body/fun.h>
+#include <core/lily/ast/data_type.h>
+#include <core/lily/ast/expr.h>
+#include <core/lily/ast/generic_param.h>
+#include <core/lily/visibility.h>
+#include <core/shared/location.h>
+
+enum LilyAstDeclFunParamKind
+{
+    LILY_AST_DECL_FUN_PARAM_KIND_DEFAULT,
+    LILY_AST_DECL_FUN_PARAM_KIND_NORMAL,
+    LILY_AST_DECL_FUN_PARAM_KIND_SELF,
+};
+
+/**
+ *
+ * @brief Convert LilyAstDeclFunParamKind in string.
+ * @note This function is only used to debug.
+ */
+#ifdef ENV_DEBUG
+char *
+IMPL_FOR_DEBUG(to_string,
+               LilyAstDeclFunParamKind,
+               enum LilyAstDeclFunParamKind self);
+#endif
+
+typedef struct LilyAstDeclFunParam
+{
+    String *name; // String*? only if kind is equal to
+                  // LILY_AST_DECL_FUN_PARAM_KIND_SELF
+    enum LilyAstDeclFunParamKind kind;
+    Location location;
+    union
+    {
+        LilyAstExpr *default_;
+    };
+} LilyAstDeclFunParam;
+
+/**
+ *
+ * @brief Construct LilyAstDeclFunParam type
+ * (LILY_AST_DECL_FUN_PARAM_KIND_DEFAULT).
+ */
+VARIANT_CONSTRUCTOR(LilyAstDeclFunParam *,
+                    LilyAstDeclFunParam,
+                    default,
+                    String *name,
+                    Location location,
+                    LilyAstExpr *default_);
+
+/**
+ *
+ * @brief Construct LilyAstDeclFunParam type
+ * (LILY_AST_DECL_FUN_PARAM_KIND_NORMAL).
+ */
+VARIANT_CONSTRUCTOR(LilyAstDeclFunParam *,
+                    LilyAstDeclFunParam,
+                    normal,
+                    String *name,
+                    Location location);
+
+/**
+ *
+ * @brief Construct LilyAstDeclFunParam type
+ * (LILY_AST_DECL_FUN_PARAM_KIND_SELF).
+ */
+VARIANT_CONSTRUCTOR(LilyAstDeclFunParam *,
+                    LilyAstDeclFunParam,
+                    self,
+                    Location location);
+
+/**
+ *
+ * @brief Convert LilyAstDeclFunParam in String.
+ * @note This function is only used to debug.
+ */
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstDeclFunParam, const LilyAstDeclFunParam *self);
+#endif
+
+/**
+ *
+ * @brief Free LilyAstDeclFunParam type.
+ */
+DESTRUCTOR(LilyAstDeclFunParam, LilyAstDeclFunParam *self);
+
+typedef struct LilyAstDeclFun
+{
+    String *name;
+    String *object_impl;               // String*?
+    Vec *generic_params;               // Vec<LilyAstGenericParam>*?
+    Vec *params;                       // Vec<LilyAstDeclFunParam*>*?
+    LilyAstDataType *return_data_type; // LilyAstDataType*?
+    Vec *body;                         // Vec<LilyAstBodyFunItem*>*
+    Vec *req;                          // Vec<LilyAstExpr*>*?
+    Vec *when;                         // Vec<LilyAstExpr*>*?
+    enum LilyVisibility visibility;
+    bool is_async;
+    bool is_operator;
+    bool req_is_comptime;
+    bool when_is_comptime;
+} LilyAstDeclFun;
+
+/**
+ *
+ * @brief Construct LilyAstDeclFun type.
+ */
+CONSTRUCTOR(LilyAstDeclFun,
+            LilyAstDeclFun,
+            String *name,
+            String *object_impl,
+            Vec *generic_params,
+            Vec *params,
+            LilyAstDataType *return_data_type,
+            Vec *body,
+            Vec *req,
+            Vec *when,
+            enum LilyVisibility visibility,
+            bool is_async,
+            bool is_operator,
+            bool req_is_comptime,
+            bool when_is_comptime);
+
+/**
+ *
+ * @brief Convert LilyAstDeclFun in String.
+ * @note This function is only used to debug.
+ */
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstDeclFun, LilyAstDeclFun *self);
+#endif
+
+/**
+ *
+ * @brief Free LilyAstDeclFun type.
+ */
+DESTRUCTOR(LilyAstDeclFun, const LilyAstDeclFun *self);
 
 #endif // LILY_CORE_LILY_AST_DECL_FUN_H
