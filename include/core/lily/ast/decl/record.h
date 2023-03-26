57a8d28b2813d2df15bf4f5dc349a0a429b0e302
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-26 19:51:20 -0400

    Add LilyAstRecord type. Add constructor, debug and destructor for this type.

---
+++ include/core/lily/ast/decl/record.h
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_CORE_LILY_AST_DECL_ENUM_H
+#define LILY_CORE_LILY_AST_DECL_ENUM_H
+
+#include <base/string.h>
+#include <base/vec.h>
+
+#include <core/lily/ast/field.h>
+#include <core/lily/visibility.h>
+
+typedef struct LilyAstDeclRecord
+{
+    String *name;
+    Vec *fields; // Vec<LilyAstField*>*
+    enum LilyVisibility visibility;
+} LilyAstDeclRecord;
+
+inline CONSTRUCTOR(LilyAstDeclRecord,
+                   LilyAstDeclRecord,
+                   String *name,
+                   Vec *fields,
+                   enum LilyVisibility visibility)
+{
+    return (LilyAstDeclRecord){ .name = name,
+                                .fields = fields,
+                                .visibility = visibility };
+}
+
+/**
+ *
+ * @brief Convert LilyAstDeclRecord in String.
+ * @note This function is only used to debug.
+ */
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstDeclRecord, const LilyAstDeclRecord *self);
+#endif
+
+/**
+ *
+ * @brief Free LilyAstDeclRecord type.
+ */
+DESTRUCTOR(LilyAstDeclRecord, const LilyAstDeclRecord *self);
+
+#endif // LILY_CORE_LILY_AST_DECL_ENUM_H
---
+++ src/core/lily/ast/decl/record.c
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <core/lily/ast/decl/record.h>
+
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstDeclRecord, const LilyAstDeclRecord *self)
+{
+    String *res =
+      format__String("LilyAstDeclRecord{{ name = {S}, fields =", self->name);
+
+    DEBUG_VEC_STRING(self->fields, res, LilyAstField);
+    push_str__String(res, ", visibility = ");
+    push_str__String(res, to_string__Debug__LilyVisibility(self->visibility));
+    push_str__String(res, " }");
+
+    return res;
+}
+#endif
+
+DESTRUCTOR(LilyAstDeclRecord, const LilyAstDeclRecord *self)
+{
+    FREE_MOVE(self->name, FREE(String, self->name));
+    FREE_BUFFER_ITEMS(self->fields->buffer, self->fields->len, LilyAstField);
+    FREE(Vec, self->fields);
+}
