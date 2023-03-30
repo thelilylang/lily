a9ec0347bc7e0f195d39618f490be9f4ab76c636
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-30 17:41:49 -0400

    Update `warning.h` and `warning.c`.

--- src/core/lily/warning.c
+++ src/core/lily/warning.c
@@ -36,6 +36,8 @@ to_msg__LilyWarning(const LilyWarning *self)
     switch (self->kind) {
         case LILY_WARNING_KIND_UNUSED_PAREN:
             return "unused paren";
+        case LILY_WARNING_KIND_UNUSED_SEMICOLON:
+            return "unused semicolon";
         default:
             UNREACHABLE("unknown variant");
     }
@@ -47,6 +49,8 @@ to_code__LilyWarning(const LilyWarning *self)
     switch (self->kind) {
         case LILY_WARNING_KIND_UNUSED_PAREN:
             return format("0001");
+        case LILY_WARNING_KIND_UNUSED_SEMICOLON:
+            return format("0002");
         default:
             UNREACHABLE("unknown variant");
     }
