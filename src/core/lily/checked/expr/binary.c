2a1b9c147c02e412c4920b5e309c4dd77a677518
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-06 23:30:42 -0400

    Update `checked/expr/binary.h` and `checked/expr/binary.c`.

---
+++ src/core/lily/checked/expr/binary.c
@@ -0,0 +1,241 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/format.h>
+#include <base/new.h>
+#include <base/string.h>
+
+#include <core/lily/checked/expr.h>
+#include <core/lily/checked/expr/binary.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef ENV_DEBUG
+char *
+IMPL_FOR_DEBUG(to_string,
+               LilyCheckedExprBinaryKind,
+               enum LilyCheckedExprBinaryKind self)
+{
+    switch (self) {
+        case LILY_CHECKED_EXPR_BINARY_KIND_ADD:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ADD";
+        case LILY_CHECKED_EXPR_BINARY_KIND_AND:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_AND";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_ADD:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_ADD";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_AND:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_AND";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_L_SHIFT:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_L_SHIFT";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_OR:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_OR";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_R_SHIFT:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_R_SHIFT";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_DIV:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_DIV";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_EXP:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_EXP";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MOD:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MOD";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MUL:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MUL";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_SUB:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_SUB";
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN";
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_AND:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_BIT_AND";
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_OR:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_BIT_OR";
+        case LILY_CHECKED_EXPR_BINARY_KIND_CHAIN:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_CHAIN";
+        case LILY_CHECKED_EXPR_BINARY_KIND_DIV:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_DIV";
+        case LILY_CHECKED_EXPR_BINARY_KIND_EXP:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_EXP";
+        case LILY_CHECKED_EXPR_BINARY_KIND_EQ:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_EQ";
+        case LILY_CHECKED_EXPR_BINARY_KIND_GREATER_EQ:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_GREATER_EQ";
+        case LILY_CHECKED_EXPR_BINARY_KIND_GREATER:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_GREATER";
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_L_SHIFT:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_L_SHIFT";
+        case LILY_CHECKED_EXPR_BINARY_KIND_LESS_EQ:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_LESS_EQ";
+        case LILY_CHECKED_EXPR_BINARY_KIND_LESS:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_LESS";
+        case LILY_CHECKED_EXPR_BINARY_KIND_MOD:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_MOD";
+        case LILY_CHECKED_EXPR_BINARY_KIND_MUL:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_MUL";
+        case LILY_CHECKED_EXPR_BINARY_KIND_NOT_EQ:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_NOT_EQ";
+        case LILY_CHECKED_EXPR_BINARY_KIND_OR:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_OR";
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_R_SHIFT:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_R_SHIFT";
+        case LILY_CHECKED_EXPR_BINARY_KIND_RANGE:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_RANGE";
+        case LILY_CHECKED_EXPR_BINARY_KIND_SUB:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_SUB";
+        case LILY_CHECKED_EXPR_BINARY_KIND_XOR:
+            return "LILY_CHECKED_EXPR_BINARY_KIND_XOR";
+        default:
+            UNREACHABLE("unknown variant");
+    }
+}
+
+char *
+IMPL_FOR_DEBUG(to_string, LilyCheckedExprBinary, const LilyCheckedExprBinary *self)
+{
+    return format("LilyCheckedExprBinary{{ kind = {s}, left = {Sr}, right = {Sr} }",
+                  to_string__Debug__LilyCheckedExprBinaryKind(self->kind),
+                  to_string__Debug__LilyCheckedExpr(self->left),
+                  to_string__Debug__LilyCheckedExpr(self->right));
+}
+#endif
+
+String *
+to_string__LilyCheckedExprBinary(const LilyCheckedExprBinary *self)
+{
+    String *res = to_string__LilyCheckedExpr(self->left);
+
+    switch (self->kind) {
+        case LILY_CHECKED_EXPR_BINARY_KIND_ADD:
+            push_str__String(res, " + ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_AND:
+            push_str__String(res, " and ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_ADD:
+            push_str__String(res, " += ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_AND:
+            push_str__String(res, " &= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_L_SHIFT:
+            push_str__String(res, " <<= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_OR:
+            push_str__String(res, " |= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_BIT_R_SHIFT:
+            push_str__String(res, " >>= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_DIV:
+            push_str__String(res, " /= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_EXP:
+            push_str__String(res, " **= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MOD:
+            push_str__String(res, " %= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_MUL:
+            push_str__String(res, " *= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_SUB:
+            push_str__String(res, " -= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN_XOR:
+            push_str__String(res, " xor= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_ASSIGN:
+            push_str__String(res, " = ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_AND:
+            push_str__String(res, " & ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_OR:
+            push_str__String(res, " | ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_CHAIN:
+            push_str__String(res, " |> ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_DIV:
+            push_str__String(res, " / ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_EQ:
+            push_str__String(res, " == ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_EXP:
+            push_str__String(res, " ** ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_GREATER_EQ:
+            push_str__String(res, " >= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_GREATER:
+            push_str__String(res, " > ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_L_SHIFT:
+            push_str__String(res, " << ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_LESS_EQ:
+            push_str__String(res, " <= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_LESS:
+            push_str__String(res, " < ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_MOD:
+            push_str__String(res, " % ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_MUL:
+            push_str__String(res, " * ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_NOT_EQ:
+            push_str__String(res, " not= ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_OR:
+            push_str__String(res, " or ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_BIT_R_SHIFT:
+            push_str__String(res, " >> ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_RANGE:
+            push_str__String(res, " .. ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_SUB:
+            push_str__String(res, " - ");
+            break;
+        case LILY_CHECKED_EXPR_BINARY_KIND_XOR:
+            push_str__String(res, " xor ");
+            break;
+        default:
+            UNREACHABLE("unknown variant");
+    }
+
+    String *right = to_string__LilyCheckedExpr(self->right);
+
+    APPEND_AND_FREE(res, right);
+
+    return res;
+}
+
+DESTRUCTOR(LilyCheckedExprBinary, const LilyCheckedExprBinary *self)
+{
+    FREE(LilyCheckedExpr, self->left);
+    FREE(LilyCheckedExpr, self->right);
+}
