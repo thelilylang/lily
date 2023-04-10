050a036cc92b3be789f9d4e3900fe04d4a33c3cf
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-10 03:42:37 -0400

    Add `pattern/list.h` and `pattern/list.c`.

---
+++ include/core/lily/ast/pattern/list.h
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, PATTERNESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_CORE_LILY_AST_PATTERN_LIST_H
+#define LILY_CORE_LILY_AST_PATTERN_LIST_H
+
+#include <base/macros.h>
+#include <base/new.h>
+#include <base/string.h>
+#include <base/vec.h>
+
+typedef struct LilyAstPatternList
+{
+    Vec *patterns; // Vec<LilyAstPattern*>*
+} LilyAstPatternList;
+
+/**
+ *
+ * @brief Construct LilyAstPatternList type.
+ */
+inline CONSTRUCTOR(LilyAstPatternList, LilyAstPatternList, Vec *patterns)
+{
+    return (LilyAstPatternList){ .patterns = patterns };
+}
+
+/**
+ *
+ * @brief Convert LilyAstPatternList in String.
+ * @note This function is only used to debug.
+ */
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstPatternList, const LilyAstPatternList *self);
+#endif
+
+/**
+ *
+ * @brief Convert LilyAstPatternList in String.
+ */
+String *
+to_string__LilyAstPatternList(const LilyAstPatternList *self);
+
+/**
+ *
+ * @brief Free LilyAstPatternList type.
+ */
+DESTRUCTOR(LilyAstPatternList, LilyAstPatternList *self);
+
+#endif // LILY_CORE_LILY_AST_PATTERN_ARRAY_H
---
+++ src/core/lily/ast/pattern/list.c
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <core/lily/ast/pattern.h>
+#include <core/lily/ast/pattern/list.h>
+
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyAstPatternList, const LilyAstPatternList *self)
+{
+    String *res = from__String("LilyAstPatternList{ patterns = { ");
+
+    DEBUG_VEC_STRING(self->patterns, res, LilyAstPattern);
+
+    push_str__String(res, "} }");
+
+    return res;
+}
+#endif
+
+String *
+to_string__LilyAstPatternList(const LilyAstPatternList *self)
+{
+    String *res = from__String("[");
+
+    for (Usize i = 0; i < self->patterns->len; i++) {
+        String *s = to_string__LilyAstPattern(get__Vec(self->patterns, i));
+
+        APPEND_AND_FREE(res, s);
+
+        if (i != self->patterns->len - 1) {
+            push_str__String(res, ", ");
+        }
+    }
+
+    push__String(res, ']');
+
+    return res;
+}
+
+DESTRUCTOR(LilyAstPatternList, LilyAstPatternList *self)
+{
+    FREE_BUFFER_ITEMS(self->patterns->buffer, self->patterns->len, LilyAstExpr);
+    FREE(Vec, self->patterns);
+}
