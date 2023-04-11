3818083712d845e3ab8abe12b10b101f4edb2947
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-10 20:49:55 -0400

    Update `generator.h` and `generator.c`.

---
+++ src/core/lily/ir/llvm/generator.c
@@ -0,0 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/macros.h>
+
+#include <core/lily/ir/llvm/builtin.h>
+#include <core/lily/ir/llvm/dump.h>
+#include <core/lily/package.h>
+
+#include <stdio.h>
+
+void
+run__LilyIrLlvmGenerator(LilyPackage *self)
+{
+    init_builtin__LilyIrLlvm(&self->ir.llvm);
+
+#ifdef ENV_DEBUG
+    printf("====LLVM IR Generator(%s)====\n", self->global_name->buffer);
+
+    dump__LilyIrLlvm(&self->ir.llvm);
+#endif
+}
