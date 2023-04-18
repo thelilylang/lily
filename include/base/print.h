dc8e47e5f03ac85620b4923a14f21de67709c438
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-18 15:40:24 -0400

    Update `print.h`.

--- include/base/print.h
+++ include/base/print.h
@@ -34,7 +34,7 @@
 #define PRINT(fmt, ...)                     \
     {                                       \
         char *f = format(fmt, __VA_ARGS__); \
-        printf("%s", f);                    \
+		fputs(f, stdout); \
         lily_free(f);                       \
     }
 
