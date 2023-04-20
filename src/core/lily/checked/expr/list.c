6a63205cc1dbe784298a295225f24b799b4d8ea7
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-20 10:00:35 -0400

    Update `list.h` and `list.c`.

---
+++ src/core/lily/checked/expr/list.c
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <core/lily/checked/expr.h>
+#include <core/lily/checked/expr/list.h>
+
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string, LilyCheckedExprList, const LilyCheckedExprList *self)
+{
+    String *res = from__String("LilyCheckedExprList{ items = { ");
+
+    DEBUG_VEC_STRING(self->items, res, LilyCheckedExpr);
+
+    push_str__String(res, "} }");
+
+    return res;
+}
+#endif
+
+String *
+to_string__LilyCheckedExprList(const LilyCheckedExprList *self)
+{
+    String *res = from__String("[");
+
+    for (Usize i = 0; i < self->items->len; i++) {
+        String *s = to_string__LilyCheckedExpr(get__Vec(self->items, i));
+
+        APPEND_AND_FREE(res, s);
+
+        if (i != self->items->len - 1) {
+            push_str__String(res, ", ");
+        }
+    }
+
+    push__String(res, ']');
+
+    return res;
+}
+
+DESTRUCTOR(LilyCheckedExprList, LilyCheckedExprList *self)
+{
+    FREE_BUFFER_ITEMS(self->items->buffer, self->items->len, LilyCheckedExpr);
+    FREE(Vec, self->items);
+}
