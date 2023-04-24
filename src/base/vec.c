ccc0135e9280a4d4b303daf0a58d123194b7c1e9
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-23 21:32:08 -0400

    Beautify file.

--- src/base/vec.c
+++ src/base/vec.c
@@ -226,13 +226,13 @@ reverse__Vec(Vec *self)
 Vec *
 slice__Vec(Vec *self, Usize start, Usize end)
 {
-	Vec *slice = NEW(Vec);
+    Vec *slice = NEW(Vec);
 
-	for (Usize i = start; i < end; ++i) {
-		push__Vec(slice, get__Vec(self, i));
-	}
+    for (Usize i = start; i < end; ++i) {
+        push__Vec(slice, get__Vec(self, i));
+    }
 
-	return slice;
+    return slice;
 }
 
 void
