05b51a7e729bf6c495fea58c8a59d9612356ff3b
ArthurPV <arthurpontiervalois@gmail.com>
2023-04-30 18:28:16 -0400

    src: Update `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ mod location;
 mod scanner;
 mod token;
 
+use std::env;
+use std::process::exit;
+
+use scanner::{Scanner, Source};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("error: please speficy the filename");
+        exit(1);
+    } else {
+        for filename in &args[1..] {
+            let filename = filename.as_str();
+            let source = Source::new(filename, Source::read(filename));
+            let mut scanner = Scanner::new(&source);
+
+            scanner.run();
+        }
+    }
 }
