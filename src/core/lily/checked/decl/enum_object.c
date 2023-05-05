9e4838a72e3ffac2ea7d7d67234c11942262c543
ArthurPV <arthurpontiervalois@gmail.com>
2023-05-05 02:40:19 -0400

    Add `LilyCheckedDeclEnumObject` type.

---
+++ src/core/lily/checked/decl/enum_object.c
@@ -0,0 +1,88 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <core/lily/checked/decl/enum_object.h>
+
+#ifdef ENV_DEBUG
+String *
+IMPL_FOR_DEBUG(to_string,
+               LilyCheckedDeclEnumObject,
+               const LilyCheckedDeclEnumObject *self)
+{
+    String *res = format__String(
+      "LilyCheckedDeclEnumObject{{ name = {S}, generic_params =", self->name);
+
+    if (self->generic_params) {
+        DEBUG_VEC_STRING(self->generic_params, res, LilyCheckedGenericParam);
+    } else {
+        push_str__String(res, " NULL");
+    }
+
+    push_str__String(res, ", impl_params =");
+
+    if (self->generic_params) {
+        DEBUG_VEC_STRING(self->impl_params, res, LilyCheckedImplParam);
+    } else {
+        push_str__String(res, " NULL");
+    }
+
+    push_str__String(res, ", body =");
+    // DEBUG_VEC_STRING(self->body, res, LilyCheckedBodyEnumObjectItem);
+
+    {
+        char *s = format(", scope = {Sr}, visibility = {s} }",
+                         to_string__Debug__LilyCheckedScope(self->scope),
+                         to_string__Debug__LilyVisibility(self->visibility));
+
+        PUSH_STR_AND_FREE(res, s);
+    }
+
+    return res;
+}
+#endif
+
+DESTRUCTOR(LilyCheckedDeclEnumObject, const LilyCheckedDeclEnumObject *self)
+{
+    FREE_MOVE(self->name, FREE(String, self->name));
+
+    if (self->generic_params) {
+        FREE_BUFFER_ITEMS(self->generic_params->buffer,
+                          self->generic_params->len,
+                          LilyCheckedGenericParam);
+        FREE(Vec, self->generic_params);
+    }
+
+    if (self->impl_params) {
+        FREE_BUFFER_ITEMS(self->impl_params->buffer,
+                          self->impl_params->len,
+                          LilyCheckedImplParam);
+        FREE(Vec, self->impl_params);
+    }
+
+    FREE_BUFFER_ITEMS(
+      self->body->buffer, self->body->len, LilyCheckedBodyEnumObjectItem);
+    FREE(Vec, self->body);
+
+    FREE(LilyCheckedScope, self->scope);
+}
