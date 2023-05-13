1ec5066aa3a0fd14d686c9dd8a0fb7c24345791f
ArthurPV <arthurpontiervalois@gmail.com>
2023-05-13 08:24:17 -0400

    Add `api.h`.

---
+++ lib/api.h
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_API_H
+#define LILY_API_H
+
+#ifdef _WIN32
+#define LILY_EXPORT __declspec(dllexport)
+#define LILY_IMPORT __declspec(dllimport)
+#else
+#define LILY_EXPORT __attribute__((visibility("default")))
+#define LILY_IMPORT __attribute__((visibility("default")))
+#endif
+
+#ifdef LILY_BUILD
+#define LILY_API LILY_EXPORT
+#else
+#define LILY_API LILY_IMPORT
+#endif
+
+#endif // LILY_API_H
