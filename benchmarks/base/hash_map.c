d5eb47553bd127073547343be1ae10c53f2e839c
ArthurPV <arthurpontiervalois@gmail.com>
2023-06-13 15:30:02 -0400

    Update HashMap bench.

--- benchmarks/base/hash_map.c
+++ benchmarks/base/hash_map.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <base/hash/fnv.h>
 #include <base/hash_map.h>
@@ -17,14 +18,13 @@ main()
         HashMap *hm = NEW(HashMap);
 
         for (Usize i = 0; i < 1000000; i++) {
-            char key[7];
+            char *key = malloc(7);
             snprintf(key, 7, "%zu", i);
 
-            insert__HashMap(hm, key, (Usize*)i);
+            insert__HashMap(hm, key, (Usize *)i);
         }
 
-		printf("%zu\n", (Usize)(Uptr)get__HashMap(hm, "2"));
-
+		// We have some memory leaks, because the keys are not free.
         FREE(HashMap, hm);
 
         printf("Time: %.2fs\n", (double)(clock() - start) / CLOCKS_PER_SEC);
