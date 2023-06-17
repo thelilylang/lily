56533e1c5d956edb3b24eea7558fc5be8de21b02
ArthurPV <arthurpontiervalois@gmail.com>
2023-06-17 15:18:00 -0400

    Update `ordered_hash_map.h` and `ordered_hash_map.c`.

---
+++ include/base/ordered_hash_map.h
@@ -0,0 +1,196 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_BASE_ORDERED_HASH_MAP_H
+#define LILY_BASE_ORDERED_HASH_MAP_H
+
+#include <base/hash_choice.h>
+#include <base/macros.h>
+#include <base/new.h>
+#include <base/platform.h>
+#include <base/types.h>
+
+#include <string.h>
+
+#ifdef PLATFORM_64
+#define HASH_MAP_SIZE INT64_MAX
+#else
+#define HASH_MAP_SIZE INT32_MAX
+#endif
+
+#define DEFAULT_ORDERED_HASH_MAP_CAPACITY 8
+
+typedef struct OrderedHashMapPair
+{
+    char *key;
+    void *value;
+    Usize id;
+} OrderedHashMapPair;
+
+/**
+ *
+ * @brief Construct OrderedHashMapPair type.
+ */
+inline CONSTRUCTOR(OrderedHashMapPair,
+                   OrderedHashMapPair,
+                   char *key,
+                   void *value,
+                   Usize id)
+{
+    return (OrderedHashMapPair){ .key = key, .value = value, .id = id };
+}
+
+typedef struct OrderedHashMapBucket
+{
+    OrderedHashMapPair pair;
+    struct OrderedHashMapBucket *next; // OrederedHashMapBucket*?
+} OrderedHashMapBucket;
+
+/**
+ *
+ * @brief Construct OrderedHashMapBucket type.
+ */
+CONSTRUCTOR(OrderedHashMapBucket *,
+            OrderedHashMapBucket,
+            OrderedHashMapPair pair);
+
+/**
+ *
+ * @brief Free OrderedHashMapBucket type.
+ */
+DESTRUCTOR(OrderedHashMapBucket, OrderedHashMapBucket *self);
+
+typedef struct OrderedHashMap
+{
+    OrderedHashMapBucket **buckets; // OrderedHashMapBucket**?
+    Usize len;
+    Usize capacity;
+} OrderedHashMap;
+
+/**
+ *
+ * @brief Construct OrderedHashMap type.
+ */
+CONSTRUCTOR(OrderedHashMap *, OrderedHashMap);
+
+/**
+ *
+ * @brief Get value by key.
+ * @return If the key does not exist, return NULL.
+ */
+void *
+get__OrderedHashMap(OrderedHashMap *self, char *key);
+
+/**
+ *
+ * @brief Get value from id.
+ * @return If the key does not exist, return NULL.
+ */
+void *
+get_from_id__OrderedHashMap(OrderedHashMap *self, Usize id);
+
+/**
+ *
+ * @brief Generate an hash.
+ */
+inline Usize
+hash__OrderedHashMap(OrderedHashMap *self, char *key)
+{
+#ifdef HASH_FNV1A
+#ifdef PLATFORM_64
+    return hash_fnv1a_64(key);
+#else
+    return hash_fnv1a_32(key);
+#endif
+#elif defined(HASH_CUSTOM)
+#ifdef PLATFORM_64
+    return hash_custom64(key);
+#else
+    return hash_custom32(key);
+#endif
+#elif defined(HASH_JENKINS)
+    return hash_jenkins(key);
+#elif defined(HASH_SIP)
+#ifdef PLATFORM_64
+    return hash_sip(
+      key, strlen(key), 0x0123456789abcdefULL, 0xfedcba9876543210ULL);
+#else
+    return hash_sip(key, strlen(key), 0x01234567, 0x89abcdef);
+#endif
+#else
+#error "cannot generate an hash"
+#endif
+}
+
+/**
+ *
+ * @brief Get index from the hash.
+ */
+inline Usize
+index__OrderedHashMap(OrderedHashMap *self, char *key)
+{
+    return hash__OrderedHashMap(self, key) % self->capacity;
+}
+
+/**
+ *
+ * @brief Insert key-value pair into OrderedHashMap.
+ * @return If the key already exists, return the value of the key, otherwise
+ * return NULL.
+ */
+void *
+insert__OrderedHashMap(OrderedHashMap *self, char *key, void *value);
+
+/**
+ *
+ * @brief Free OrderedHashMap type.
+ */
+DESTRUCTOR(OrderedHashMap, OrderedHashMap *self);
+
+typedef struct OrderedHashMapIter
+{
+    OrderedHashMap *ordered_hash_map;
+    Usize count;
+} OrderedHashMapIter;
+
+/**
+ *
+ * @brief Construct OrderedHashMapIter type.
+ */
+inline CONSTRUCTOR(OrderedHashMapIter,
+                   OrderedHashMapIter,
+                   OrderedHashMap *ordered_hash_map)
+{
+    return (OrderedHashMapIter){ .ordered_hash_map = ordered_hash_map,
+                                 .count = 0 };
+}
+
+/**
+ *
+ * @brief Get the next value.
+ */
+void *
+next__OrderedHashMapIter(OrderedHashMapIter *self);
+
+#endif // LILY_BASE_ORDERED_HASH_MAP_H
---
+++ src/base/ordered_hash_map.c
@@ -0,0 +1,218 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/alloc.h>
+#include <base/ordered_hash_map.h>
+
+#include <stdlib.h>
+
+static void *
+get_bucket__OrderedHashMapBucket(const OrderedHashMapBucket *self, char *key);
+
+static void *
+push_bucket__OrderedHashMap(OrderedHashMap *self,
+                            Usize index,
+                            OrderedHashMapBucket *new);
+
+CONSTRUCTOR(OrderedHashMapBucket *,
+            OrderedHashMapBucket,
+            OrderedHashMapPair pair)
+{
+    OrderedHashMapBucket *self = lily_malloc(sizeof(OrderedHashMapBucket));
+
+    self->pair = pair;
+    self->next = NULL;
+
+    return self;
+}
+
+DESTRUCTOR(OrderedHashMapBucket, OrderedHashMapBucket *self)
+{
+    if (self->next) {
+        FREE(OrderedHashMapBucket, self->next);
+    }
+
+    lily_free(self);
+}
+
+CONSTRUCTOR(OrderedHashMap *, OrderedHashMap)
+{
+    OrderedHashMap *self = lily_malloc(sizeof(OrderedHashMap));
+
+    self->buckets = NULL;
+    self->len = 0;
+    self->capacity = DEFAULT_ORDERED_HASH_MAP_CAPACITY;
+
+    return self;
+}
+
+void *
+get_bucket__OrderedHashMapBucket(const OrderedHashMapBucket *self, char *key)
+{
+    if (!strcmp(self->pair.key, key)) {
+        return self->pair.value;
+    }
+
+    return self->next ? get_bucket__OrderedHashMapBucket(self->next, key)
+                      : NULL;
+}
+
+void *
+get__OrderedHashMap(OrderedHashMap *self, char *key)
+{
+    Usize index = index__OrderedHashMap(self, key);
+    OrderedHashMapBucket *bucket = self->buckets[index];
+
+    return bucket ? get_bucket__OrderedHashMapBucket(bucket, key) : NULL;
+}
+
+void *
+get_from_id__OrderedHashMap(OrderedHashMap *self, Usize id)
+{
+    for (Usize i = 0; i < self->len; ++i) {
+        OrderedHashMapBucket *current_bucket = self->buckets[i];
+
+        while (current_bucket->next) {
+            if (current_bucket->pair.id == id) {
+                return current_bucket->pair.value;
+            }
+
+            current_bucket = self->buckets[i]->next;
+        }
+
+        if (current_bucket->pair.id == id) {
+            return current_bucket->pair.value;
+        }
+    }
+
+    return NULL;
+}
+
+void *
+push_bucket__OrderedHashMap(OrderedHashMap *self,
+                            Usize index,
+                            OrderedHashMapBucket *new)
+{
+    void *is_exist = get__OrderedHashMap(self, new->pair.key);
+
+    if (is_exist)
+        return is_exist;
+
+    if (self->buckets[index]) {
+        OrderedHashMapBucket *current = self->buckets[index];
+
+        while (current->next) {
+            current = current->next;
+        }
+
+        current->next = new;
+
+        return NULL;
+    }
+
+    self->buckets[index] = new;
+
+    return NULL;
+}
+
+void *
+insert__OrderedHashMap(OrderedHashMap *self, char *key, void *value)
+{
+    Usize index = index__OrderedHashMap(self, key);
+
+    if (!self->buckets) {
+        self->buckets =
+          lily_calloc(DEFAULT_ORDERED_HASH_MAP_CAPACITY, PTR_SIZE);
+        self->buckets[index] =
+          NEW(OrderedHashMapBucket,
+              NEW(OrderedHashMapPair, key, value, self->len++));
+
+        return NULL;
+    }
+
+    if (self->len + 1 > self->capacity) {
+        self->capacity *= 2;
+
+        OrderedHashMapBucket **new_buckets =
+          lily_calloc(self->capacity, PTR_SIZE);
+        OrderedHashMapBucket **old_buckets = self->buckets;
+
+        self->buckets = new_buckets;
+
+        // Re-hash all inserted K-V
+        for (Usize i = 0; i < self->len; ++i) {
+            OrderedHashMapBucket *current = old_buckets[i];
+
+            while (current != NULL) {
+                OrderedHashMapBucket *next = current->next;
+                Usize new_index =
+                  index__OrderedHashMap(self, current->pair.key);
+
+                current->next = new_buckets[new_index];
+                new_buckets[new_index] = current;
+                current = next;
+            }
+        }
+
+        lily_free(old_buckets);
+
+        // Reload index
+        index = index__OrderedHashMap(self, key);
+    }
+
+    void *is_exist = push_bucket__OrderedHashMap(
+      self,
+      index,
+      NEW(OrderedHashMapBucket,
+          NEW(OrderedHashMapPair, key, value, self->len)));
+
+    if (is_exist) {
+        return is_exist;
+    }
+
+    ++self->len;
+
+    return NULL;
+}
+
+DESTRUCTOR(OrderedHashMap, OrderedHashMap *self)
+{
+    if (self->buckets) {
+        for (Usize i = 0; i < self->capacity; ++i) {
+            if (self->buckets[i]) {
+                FREE(OrderedHashMapBucket, self->buckets[i]);
+            }
+        }
+
+        lily_free(self->buckets);
+    }
+
+    lily_free(self);
+}
+
+void *
+next__OrderedHashMapIter(OrderedHashMapIter *self)
+{
+    return get_from_id__OrderedHashMap(self->ordered_hash_map, self->count++);
+}
