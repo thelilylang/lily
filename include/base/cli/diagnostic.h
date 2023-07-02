00a85456578dbfc6e57afead5a64b86dc62aa94b
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-02 02:58:29 -0400

    Add `diagnostic.h` and `diagnostic.c`.

---
+++ include/base/cli/diagnostic.h
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_BASE_CLI_DIAGNOSTIC_H
+#define LILY_BASE_CLI_DIAGNOSTIC_H
+
+#include <base/string.h>
+#include <base/types.h>
+
+enum CliDiagnosticKind
+{
+    CLI_DIAGNOSTIC_KIND_ERROR,
+    CLI_DIAGNOSTIC_KIND_NOTE,
+    CLI_DIAGNOSTIC_KIND_WARNING,
+};
+
+typedef struct CliDiagnostic
+{
+    enum CliDiagnosticKind kind;
+    const char *msg;
+    Usize arg_count;
+    String *full_command; // String* (&)
+} CliDiagnostic;
+
+/**
+ *
+ * @brief Construct CliDiagnostic type.
+ */
+inline CONSTRUCTOR(CliDiagnostic,
+                   CliDiagnostic,
+                   enum CliDiagnosticKind kind,
+                   const char *msg,
+                   Usize arg_count,
+                   String *full_command)
+{
+    return (CliDiagnostic){ .kind = kind,
+                            .msg = msg,
+                            .arg_count = arg_count,
+                            .full_command = full_command };
+}
+
+/**
+ *
+ * @brief Emit a diagnostic (can exit(1) when it emits an error).
+ */
+void
+emit__CliDiagnostic(const CliDiagnostic *self);
+
+#endif // LILY_BASE_CLI_DIAGNOSTIC_H
---
+++ src/base/cli/diagnostic.c
@@ -0,0 +1,105 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/cli/diagnostic.h>
+#include <base/color.h>
+#include <base/new.h>
+#include <base/print.h>
+
+#include <stdio.h>
+
+static void
+print_detail__CliDiagnostic(const CliDiagnostic *self);
+
+void
+print_detail__CliDiagnostic(const CliDiagnostic *self)
+{
+    PRINTLN("\x1b[1m{Sr}\x1b[0m", repeat__String("-", self->full_command->len));
+    PRINTLN("{sa}", BOLD(">"));
+    PRINTLN("{sa}\x1b[1m{S}\x1b[0m", BOLD(">"), self->full_command);
+    PRINT("\x1b[1m> ");
+
+    {
+        Usize count_space = 0;
+        StringIter iter = NEW(StringIter, self->full_command);
+        char current = '\0';
+
+        while (count_space < self->arg_count) {
+            current = next__StringIter(&iter);
+
+            if (current) {
+                if (current == ' ') {
+                    PRINT(" ");
+                    count_space += 1;
+                } else if (self->arg_count == count_space + 1) {
+                    PRINT("^");
+                } else {
+                    PRINT(" ");
+                }
+            } else {
+                UNREACHABLE("invalid argument count");
+            }
+        }
+    }
+
+    PRINTLN("\x1b[0m");
+    PRINTLN("\x1b[1m{Sr}\x1b[0m", repeat__String("-", self->full_command->len));
+}
+
+void
+emit__CliDiagnostic(const CliDiagnostic *self)
+{
+    switch (self->kind) {
+        case CLI_DIAGNOSTIC_KIND_ERROR:
+            PRINTLN("{sa}{sa}\x1b[1m{s}\x1b[0m",
+                    RED_BOLD("error"),
+                    BOLD(":"),
+                    self->msg);
+            break;
+        case CLI_DIAGNOSTIC_KIND_NOTE:
+            PRINTLN("{sa}{sa}\x1b[1m{s}\x1b[0m",
+                    CYAN_BOLD("note"),
+                    BOLD(":"),
+                    self->msg);
+            break;
+        case CLI_DIAGNOSTIC_KIND_WARNING:
+            PRINTLN("{sa}{sa}\x1b[1m{s}\x1b[0m",
+                    YELLOW_BOLD("warning"),
+                    BOLD(":"),
+                    self->msg);
+            break;
+        default:
+            UNREACHABLE("unknown variant");
+    }
+
+    PRINTLN("{sa}\n", CYAN_BOLD("---> at(cli)"));
+    print_detail__CliDiagnostic(self);
+
+    switch (self->kind) {
+        case CLI_DIAGNOSTIC_KIND_ERROR:
+            exit(1);
+        default:
+            break;
+    }
+}
