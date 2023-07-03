97a60ae6b14c2d56e423c6872813a7fec3696953
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-03 02:58:53 -0400

    Finish to generate an help for cli and subcommand.

--- src/base/cli/help.c
+++ src/base/cli/help.c
@@ -28,10 +28,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GENERATE_USAGE(options, value)                                       \
+    {                                                                        \
+        String *usage = format__String(                                      \
+          "Usage: {s}{s}", cli->name, options->len > 0 ? " [options]" : ""); \
+                                                                             \
+        if (value) {                                                         \
+            push_str__String(usage, " [");                                   \
+            push_str__String(usage, (char *)value->name);                    \
+            push__String(usage, ']');                                        \
+                                                                             \
+            if (value->kind == CLI_VALUE_KIND_MULTIPLE) {                    \
+                push_str__String(usage, "...");                              \
+            }                                                                \
+        }                                                                    \
+                                                                             \
+        APPEND_AND_FREE(res, usage);                                         \
+        push_str__String(res, "\n\n");                                       \
+    }
+
+#define GENERATE_OPTIONS(ops)                                            \
+    if (ops->len > 0) {                                                  \
+        String *options = from__String("Options:\n\n");                  \
+        OrderedHashMapIter iter = NEW(OrderedHashMapIter, ops);          \
+        CliOption *current = NULL;                                       \
+                                                                         \
+        while ((current = next__OrderedHashMapIter(&iter))) {            \
+            /* Determine if is a short option (to avoid duplication) */  \
+            {                                                            \
+                OrderedHashMapPair *pair =                               \
+                  get_pair_from_id__OrderedHashMap(ops, iter.count - 1); \
+                                                                         \
+                ASSERT(pair);                                            \
+                                                                         \
+                if (strlen(pair->key) == 2) {                            \
+                    continue;                                            \
+                }                                                        \
+            }                                                            \
+                                                                         \
+            String *option = format__String("  {s}", current->name);     \
+                                                                         \
+            if (current->short_name) {                                   \
+                push_str__String(option, ", ");                          \
+                push_str__String(option, current->short_name);           \
+            }                                                            \
+                                                                         \
+            if (current->help) {                                         \
+                push_str__String(option, "\t\t");                        \
+                push_str__String(option, current->help);                 \
+                push__String(option, '\n');                              \
+            }                                                            \
+                                                                         \
+            APPEND_AND_FREE(options, option);                            \
+        }                                                                \
+                                                                         \
+        APPEND_AND_FREE(res, options);                                   \
+    }
+
 String *
 generate_help__CliHelp(const Cli *cli, const CliCommand *cmd)
 {
-    ASSERT((cli || cmd) && !(cli && cmd));
+    ASSERT((cli && !cmd) || (cli && cmd));
 
-    return from__String("help");
+    String *res = NEW(String);
+
+    if (cmd) {
+        GENERATE_USAGE(cmd->options, cmd->value);
+        GENERATE_OPTIONS(cmd->options);
+    } else {
+        GENERATE_USAGE(cli->options, cli->value);
+
+        if (cli->subcommands->len > 0) {
+            String *subcommands = from__String("Commands:\n\n");
+            OrderedHashMapIter iter = NEW(OrderedHashMapIter, cli->subcommands);
+            CliCommand *current = NULL;
+
+            while ((current = next__OrderedHashMapIter(&iter))) {
+                String *subcommand = format__String("  {s}", current->name);
+
+                if (current->help) {
+                    push_str__String(subcommand, "\t\t");
+                    push_str__String(subcommand, current->help);
+                    push__String(subcommand, '\n');
+                }
+
+                APPEND_AND_FREE(subcommands, subcommand);
+            }
+
+            APPEND_AND_FREE(res, subcommands);
+            push_str__String(res, "\n");
+        }
+
+        GENERATE_OPTIONS(cli->options);
+    }
+
+    return res;
 }
