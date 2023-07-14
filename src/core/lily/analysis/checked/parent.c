ded97a7337ace17fb7987b979f54f97c29021863
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-14 01:15:30 -0400

    Update `parent.c`.

--- src/core/lily/analysis/checked/parent.c
+++ src/core/lily/analysis/checked/parent.c
@@ -45,6 +45,8 @@ IMPL_FOR_DEBUG(to_string,
             return "LILY_CHECKED_PARENT_KIND_MODULE";
         case LILY_CHECKED_PARENT_KIND_SCOPE:
             return "LILY_CHECKED_PARENT_KIND_SCOPE";
+        case LILY_CHECKED_PARENT_KIND_STMT:
+            return "LILY_CHECKED_PARENT_KIND_STMT";
         default:
             UNREACHABLE("unknown variant");
     }
