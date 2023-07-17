c63aa54d2530f777cd5a1f326cfd866a3c527055
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-17 13:57:51 -0400

    Update `api.h`.

--- include/base/memory/api.h
+++ include/base/memory/api.h
@@ -25,12 +25,47 @@
 #ifndef LILY_BASE_MEMORY_API_H
 #define LILY_BASE_MEMORY_API_H
 
-#define MEMORY_API_ALIGN(mem, align)                         \
-    ASSERT(align % 2 == 0);                                  \
-                                                             \
-    Uptr addr = (Uptr)mem;                                   \
-    Uptr aligned_addr = (addr + (align - 1)) & ~(align - 1); \
-                                                             \
-    return (void *)aligned_addr;
+#include <builtin/alloc.h>
+
+/**
+ *
+ * @brief Align pointer with the given size alignment.
+ */
+inline void *
+align__MemoryApi(void *mem, Usize align)
+{
+    return __align__$Alloc(mem, align);
+}
+
+/**
+ *
+ * @brief Alloc to a pointer with the given size + align the allocated pointer.
+ */
+inline void *
+alloc__MemoryApi(Usize size, Usize align)
+{
+    return __alloc__$Alloc(size, align);
+}
+
+/**
+ *
+ * @brief Resize the given pointer with the new_size + align the resized
+ * pointer.
+ */
+inline void *
+resize__MemoryApi(void *mem, Usize new_size, Usize align)
+{
+    return __resize__$Alloc(mem, new_size, align);
+}
+
+/**
+ *
+ * @brief Free the given pointer according the given size and alignment.
+ */
+inline void
+free__MemoryApi(void **mem, Usize size, Usize align)
+{
+    return __free__$Alloc(mem, size, align);
+}
 
 #endif // LILY_BASE_MEMORY_API_H
