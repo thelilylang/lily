93d5c2ca68f1ffed3ac1d6fbf0b406ec0bd22ae9
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-23 23:04:51 -0400

    Update `test_samples.py`.

--- scripts/test_samples.py
+++ scripts/test_samples.py
@@ -7,9 +7,10 @@ from termcolor import colored
 
 FILES = [y for x in os.walk("./tests/samples") for y in glob(os.path.join(x[0], '*.lily'))]
 COMMAND = "./build/Debug/lilyc"
+FILES_EXCLUDED = ["./tests/samples/package/a.lily", "./tests/samples/package/b.lily"]
 
 def print_summary(success: int, failed: int) -> None:
-    sys.stdout.write(f"\r\x1b[31mfailed: {failed}\x1b[0m, \x1b[32msuccess: {success}\x1b[0m, \x1b[30mtotal: {len(FILES)}\x1b[0m")
+    sys.stdout.write(f"\r\x1b[31mfailed: {failed}\x1b[0m, \x1b[32msuccess: {success}\x1b[0m, \x1b[30mtotal: {len(FILES) - len(FILES_EXCLUDED)}\x1b[0m")
     sys.stdout.flush()
 
 if __name__ == "__main__":
@@ -19,6 +20,9 @@ if __name__ == "__main__":
     print_summary(success, failed)
 
     for file in FILES:
+        if file in FILES_EXCLUDED:
+           continue
+
         status = os.system(COMMAND + " " + file)
 
         if status == 0:
