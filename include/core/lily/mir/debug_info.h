2f96628ef99b91cea6dfba1e7da4ab2f234bab7e
ArthurPV <arthurpontiervalois@gmail.com>
2023-07-31 13:19:52 -0400

    Update `debug_info.h`.

--- include/core/lily/mir/debug_info.h
+++ include/core/lily/mir/debug_info.h
@@ -29,61 +29,70 @@
 
 typedef struct LilyMirDebugInfo LilyMirDebugInfo;
 
-typedef struct LilyMirDebugInfoFile {
-	String *filename;
-	String *directory;
+typedef struct LilyMirDebugInfoFile
+{
+    String *filename;
+    String *directory;
 } LilyMirDebugInfoFile;
 
-typedef struct LilyMirDebugInfoBlock {
-	LilyMirDebugInfo *scope; // LilyMirDebugInfo* (&)
-	LilyMirDebugInfoFile *file; // LilyMirDebugInfoFile* (&)
-	Usize line;
-	Usize column;
+typedef struct LilyMirDebugInfoBlock
+{
+    LilyMirDebugInfo *scope;    // LilyMirDebugInfo* (&)
+    LilyMirDebugInfoFile *file; // LilyMirDebugInfoFile* (&)
+    Usize line;
+    Usize column;
 } LilyMirDebugInfoBlock;
 
-typedef struct LilyMirDebugInfoLocation {
-	LilyMirDebugInfo *scope; // LilyMirDebugInfo* (&)
-	Usize line;
-	Usize column;
+typedef struct LilyMirDebugInfoLocation
+{
+    LilyMirDebugInfo *scope; // LilyMirDebugInfo* (&)
+    Usize line;
+    Usize column;
 } LilyMirDebugInfoLocation;
 
-typedef struct LilyMirDebugInfoSubProgram {
-	LilyMirDebugInfo *scope; // LilyMirDebugInfo* (&)
-	LilyMirDebugInfoFile *file; // LilyMirDebugInfoFile* (&)
-	Usize line;
-	Usize column;
+typedef struct LilyMirDebugInfoSubProgram
+{
+    LilyMirDebugInfo *scope;    // LilyMirDebugInfo* (&)
+    LilyMirDebugInfoFile *file; // LilyMirDebugInfoFile* (&)
+    Usize line;
+    Usize column;
 } LilyMirDebugInfoSubProgram;
 
-typedef struct LilyMirDebugInfoEnumerator {
-	const char *name;
-	Usize value;
-	bool is_unsigned;
+typedef struct LilyMirDebugInfoEnumerator
+{
+    const char *name;
+    Usize value;
+    bool is_unsigned;
 } LilyMirDebugInfoEnumerator;
 
-typedef struct LilyMirDebugInfoGlobalVariable {
+typedef struct LilyMirDebugInfoGlobalVariable
+{
 } LilyMirDebugInfoGlobalVariable;
 
-enum LilyMirDebugInfoKind {
-	LILY_MIR_DEBUG_INFO_KIND_FILE,
-	LILY_MIR_DEBUG_INFO_KIND_BLOCK,
-	LILY_MIR_DEBUG_INFO_KIND_LOCATION,
-	LILY_MIR_DEBUG_INFO_KIND_SUB_PROGRAM,
-	LILY_MIR_DEBUG_INFO_KIND_ENUMERATOR,
-	LILY_MIR_DEBUG_INFO_KIND_GLOBAL_VARIABLE,
-	LILY_MIR_DEBUG_INFO_KIND_LOCAL_VARIABLE,
-	LILY_MIR_DEBUG_INFO_KIND_EXPRESSION,
+enum LilyMirDebugInfoKind
+{
+    LILY_MIR_DEBUG_INFO_KIND_FILE,
+    LILY_MIR_DEBUG_INFO_KIND_BLOCK,
+    LILY_MIR_DEBUG_INFO_KIND_LOCATION,
+    LILY_MIR_DEBUG_INFO_KIND_SUB_PROGRAM,
+    LILY_MIR_DEBUG_INFO_KIND_ENUMERATOR,
+    LILY_MIR_DEBUG_INFO_KIND_GLOBAL_VARIABLE,
+    LILY_MIR_DEBUG_INFO_KIND_LOCAL_VARIABLE,
+    LILY_MIR_DEBUG_INFO_KIND_EXPRESSION,
 };
 
-typedef struct LilyMirDebugInfo {
-	enum LilyMirDebugInfoKind kind;
-	Usize id;
-	union {
-		LilyMirDebugInfoFile file;
-		LilyMirDebugInfoBlock block;
-		LilyMirDebugInfoLocation location;
-		LilyMirDebugInfoSubProgram sub_program;
-		LilyMirDebugInfoEnumerator enumerator;
-	};
+typedef struct LilyMirDebugInfo
+{
+    enum LilyMirDebugInfoKind kind;
+    Usize id;
+    union
+    {
+        LilyMirDebugInfoFile file;
+        LilyMirDebugInfoBlock block;
+        LilyMirDebugInfoLocation location;
+        LilyMirDebugInfoSubProgram sub_program;
+        LilyMirDebugInfoEnumerator enumerator;
+    };
 } LilyMirDebugInfo;
 
 #endif // LILY_CORE_LILY_MIR_DEBUG_INFO_H
