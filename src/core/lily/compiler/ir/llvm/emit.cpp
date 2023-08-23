7f927ba90abdd1ad10c7fe2a0f053461920f0b8a
ArthurPV <arthurpontiervalois@gmail.com>
2023-08-23 03:52:49 -0400

    Update `emit.cpp`.

--- src/core/lily/compiler/ir/llvm/emit.cpp
+++ src/core/lily/compiler/ir/llvm/emit.cpp
@@ -43,100 +43,63 @@ LilyLLVMEmit(const LilyIrLlvm *self,
              bool emit_ir,
              bool emit_bitcode)
 {
-    // if (LLVMVerifyModule(self->module, LLVMPrintMessageAction, error_msg)) {
-    //     return 1;
-    // }
-
-    raw_fd_ostream *out_obj_ptr = nullptr;
-    raw_fd_ostream *out_asm_ptr = nullptr;
-    raw_fd_ostream *out_bc_ptr = nullptr;
-
-    if (emit_asm) {
-        std::error_code ec;
-        out_asm_ptr =
-          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
-
-        if (ec) {
-            *error_msg =
-              strdup((const char *)StringRef(ec.message()).bytes_begin());
-
-            return 1;
-        }
-    }
-
-    if (emit_obj) {
-        std::error_code ec;
-        out_obj_ptr =
-          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
-
-        if (ec) {
-            *error_msg =
-              strdup((const char *)StringRef(ec.message()).bytes_begin());
-
-            return 1;
-        }
-    }
-
-    if (emit_bitcode) {
-        std::error_code ec;
-        out_bc_ptr =
-          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
-
-        if (ec) {
-            *error_msg =
-              strdup((const char *)StringRef(ec.message()).bytes_begin());
-
-            return 1;
-        }
-    }
-
-    std::unique_ptr<raw_fd_ostream> out_asm(out_asm_ptr), out_obj(out_obj_ptr),
-      out_bc(out_bc_ptr);
-    auto &module = *unwrap(self->module);
-    auto &machine = *reinterpret_cast<TargetMachine *>(self->machine);
-
-    legacy::PassManager cpm;
-
-    cpm.add(
-      createTargetTransformInfoWrapperPass(machine.getTargetIRAnalysis()));
-
-    if (emit_obj) {
-        if (machine.addPassesToEmitFile(
-              cpm, *out_obj, nullptr, CGFT_ObjectFile)) {
-            *error_msg = strdup("Can't emit an object file");
-            return 1;
-        }
-    }
-
-    if (emit_asm) {
-        if (machine.addPassesToEmitFile(
-              cpm, *out_asm, nullptr, CGFT_AssemblyFile)) {
-            *error_msg = strdup("Can't emit assembly file");
-            return 1;
-        }
-    }
-
-    // CodeGen phase
-    // cpm.run(module);
-
     if (emit_ir) {
         if (LLVMPrintModuleToFile(self->module, filename, error_msg)) {
             return 1;
         }
+
+        return 0;
     }
 
-    if (emit_obj) {
-        if (LLVMTargetMachineEmitToFile(self->machine,
-                                        self->module,
-                                        filename,
-                                        LLVMObjectFile,
-                                        error_msg)) {
+    auto &module = *unwrap(self->module);
+    auto &machine = *reinterpret_cast<TargetMachine *>(self->machine);
+    std::error_code ec;
+    legacy::PassManager code_gen_passes;
+
+    if (emit_asm) {
+        auto *out_asm =
+          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
+
+        if (ec) {
+            *error_msg =
+              strdup((const char *)StringRef(ec.message()).bytes_begin());
+
             return 1;
         }
-    }
 
-    if (emit_bitcode) {
+        machine.addPassesToEmitFile(
+          code_gen_passes, *out_asm, nullptr, llvm::CGFT_ObjectFile);
+        code_gen_passes.run(module);
+        out_asm->flush();
+    } else if (emit_obj) {
+        auto *out_obj =
+          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
+
+        if (ec) {
+            *error_msg =
+              strdup((const char *)StringRef(ec.message()).bytes_begin());
+
+            return 1;
+        }
+
+        machine.addPassesToEmitFile(
+          code_gen_passes, *out_obj, nullptr, llvm::CGFT_AssemblyFile);
+        code_gen_passes.run(module);
+        out_obj->flush();
+    } else if (emit_bitcode) {
+        auto *out_bc =
+          new (std::nothrow) raw_fd_ostream(filename, ec, sys::fs::OF_None);
+
+        if (ec) {
+            *error_msg =
+              strdup((const char *)StringRef(ec.message()).bytes_begin());
+
+            return 1;
+        }
+
         WriteBitcodeToFile(module, *out_bc);
+        code_gen_passes.run(module);
+        out_bc->flush();
     }
 
     return 0;
