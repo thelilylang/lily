aac5d25342e63ac3382fd0cf922548d5f712a1f1
ArthurPV <arthurpontiervalois@gmail.com>
2023-09-04 18:41:08 -0400

    Update `new.h`.

--- include/base/new.h
+++ include/base/new.h
@@ -25,10 +25,44 @@
 #ifndef LILY_BASE_NEW_H
 #define LILY_BASE_NEW_H
 
+#include <base/macros.h>
+#include <base/types.h>
+
 #define NEW(name, ...) __new__##name(__VA_ARGS__)
 #define NEW_VARIANT(name, variant, ...) __new__##name##__##variant(__VA_ARGS__)
 #define FREE(name, ...) __free__##name(__VA_ARGS__)
 #define FREE_VARIANT(name, variant, ...) \
     __free__##name##__##variant(__VA_ARGS__)
+
+inline DESTRUCTOR(Int8, Int8) {}
+inline DESTRUCTOR(Int16, Int16) {}
+inline DESTRUCTOR(Int32, Int32) {}
+inline DESTRUCTOR(Int64, Int64) {}
+
+inline DESTRUCTOR(Uint8, Uint8) {}
+inline DESTRUCTOR(Uint16, Uint16) {}
+inline DESTRUCTOR(Uint32, Uint32) {}
+inline DESTRUCTOR(Uint64, Uint64) {}
+
+inline DESTRUCTOR(Isize, Isize) {}
+inline DESTRUCTOR(Usize, Usize) {}
+
+inline DESTRUCTOR(Float32, Float32) {}
+inline DESTRUCTOR(Float64, Float64) {}
+
+inline DESTRUCTOR(Uptr, Uptr) {}
+
+inline DESTRUCTOR(Bool, Bool) {}
+inline DESTRUCTOR(Char, Char) {}
+inline DESTRUCTOR(Short, Short) {}
+inline DESTRUCTOR(Int, Int) {}
+inline DESTRUCTOR(Long, Long) {}
+inline DESTRUCTOR(Longlong, Longlong) {}
+
+inline DESTRUCTOR(Uchar, Uchar) {}
+inline DESTRUCTOR(Ushort, Ushort) {}
+inline DESTRUCTOR(Uint, Uint) {}
+inline DESTRUCTOR(Ulong, Ulong) {}
+inline DESTRUCTOR(Ulonglong, Ulonglong) {}
 
 #endif // LILY_BASE_NEW_H
