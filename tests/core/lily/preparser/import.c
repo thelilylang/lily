0ee765f0500d1e95f8834c8326e8fbfd1bbb2139
ArthurPV <arthurpontiervalois@gmail.com>
2023-09-05 19:03:18 -0400

    Update `import` test.

--- tests/core/lily/preparser/import.c
+++ tests/core/lily/preparser/import.c
@@ -6,15 +6,13 @@ SIMPLE(import, {
     RUN_PREPARSER(FILE_IMPORT);
 
     PRIVATE_IMPORT_ITERATOR();
+    PUBLIC_IMPORT_ITERATOR();
 
     TEST_ASSERT_EQ(CURRENT_PRIVATE_IMPORT()->as, NULL);
     TEST_ASSERT(!strcmp(CURRENT_PRIVATE_IMPORT()->value->buffer, "@sys"));
 
+    TEST_ASSERT(!strcmp(CURRENT_PUBLIC_IMPORT()->as->buffer, "s"));
+    TEST_ASSERT(!strcmp(CURRENT_PUBLIC_IMPORT()->value->buffer, "@std"));
+
     FREE_PREPARSER();
-    // SCANNER_ITERATOR();
-
-    // TEST_ASSERT_EQ(CURRENT()->kind, LILY_TOKEN_KIND_LITERAL_CSTR);
-    // TEST_ASSERT(!strcmp(NEXT()->literal_cstr, "Hello"));
-
-    // FREE_SCANNER();
 });
