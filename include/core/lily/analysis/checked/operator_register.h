cb12e6e190db6f6ea7432f50c0dec8e32c3acf63
ArthurPV <arthurpontiervalois@gmail.com>
2023-09-12 09:03:30 -0400

    Update `operator_register.h`.

--- include/core/lily/analysis/checked/operator_register.h
+++ include/core/lily/analysis/checked/operator_register.h
@@ -29,7 +29,7 @@
 
 typedef struct LilyCheckedOperatorRegister
 {
-    Vec *operators; // Vec<LilyCheckedOperator* (&) | LilyCheckedOperator*>*
+    Vec *operators; // Vec<LilyCheckedOperator*>*
 } LilyCheckedOperatorRegister;
 
 /**
