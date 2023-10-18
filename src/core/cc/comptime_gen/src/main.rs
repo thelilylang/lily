64e2d80b7a50fb0e8607c6f2e81cf874f73e1e38
ArthurPV <arthurpontiervalois@gmail.com>
2023-10-18 10:30:42 -0400

    Update cc_comptime_gen.

--- src/core/cc/comptime_gen/src/main.rs
+++ src/core/cc/comptime_gen/src/main.rs
@@ -6,10 +6,13 @@ mod parser;
 mod scanner;
 mod token;
 
+use std::cell::RefCell;
 use std::env;
 use std::process::exit;
+use std::rc::Rc;
 
 use generator::Generator;
+use parser::Parser;
 use scanner::{Scanner, Source};
 
 fn main() {
@@ -22,11 +25,13 @@ fn main() {
         for filename in &args[1..] {
             let filename = filename.as_str();
             let source = Source::new(filename, Source::read(filename));
-            let mut scanner = Scanner::new(&source);
+            let scanner = Rc::new(RefCell::new(Scanner::new(&source)));
+            let mut parser = Parser::new(Rc::clone(&scanner));
 
-            scanner.run();
+            scanner.borrow_mut().run();
+            parser.run();
 
-            let mut generator = Generator::new(&source, &scanner);
+            let mut generator = Generator::new(&source, Rc::clone(&scanner), &parser);
 
             generator.run();
         }
--- src/core/cc/comptime_gen/src/parser.rs
+++ src/core/cc/comptime_gen/src/parser.rs
@@ -1,1 +1,16 @@
+use crate::scanner::Scanner;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct Parser<'a> {
+    pub scanner: Rc<RefCell<Scanner<'a>>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(scanner: Rc<RefCell<Scanner<'a>>>) -> Self {
+        Self { scanner }
+    }
+
+    pub fn run(&mut self) {}
+}
