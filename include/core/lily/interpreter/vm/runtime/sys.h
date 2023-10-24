d4ac5f2121f4c5cf1161d641a31c2777b53820af
ArthurPV <arthurpontiervalois@gmail.com>
2023-10-24 01:33:12 -0400

    feat(lily/interpreter): Call write function syscall with runtime value.

---
+++ include/core/lily/interpreter/vm/runtime/sys.h
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2023 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LILY_CORE_LILY_INTERPRETER_RUNTIME_SYS_H
+#define LILY_CORE_LILY_INTERPRETER_RUNTIME_SYS_H
+
+#include <core/lily/interpreter/vm/value.h>
+
+/**
+ *
+ * @brief Run write system call to the runtime.
+ */
+LilyInterpreterValue *
+write__LilyInterpreterVMRuntimeSys(LilyInterpreterValue *fd,
+                                   LilyInterpreterValue *buf,
+                                   LilyInterpreterValue *n);
+
+#endif // LILY_CORE_LILY_INTERPRETER_RUNTIME_SYS_H
