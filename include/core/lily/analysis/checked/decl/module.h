ba33e22d29c01ce71b21047007ff13c7271cab05
ArthurPV <arthurpontiervalois@gmail.com>
2023-11-21 09:09:09 -0500

    refactor(lily/analysis): Update `module.h` and `module.c`.

--- include/core/lily/analysis/checked/decl/module.h
+++ include/core/lily/analysis/checked/decl/module.h
@@ -27,32 +27,16 @@
 
 #include <base/macros.h>
 #include <base/string.h>
-#include <base/vec.h>
 
-#include <core/lily/analysis/checked/access.h>
+#include <core/lily/analysis/checked/scope.h>
 #include <core/lily/shared/visibility.h>
-
-typedef struct LilyCheckedDecl LilyCheckedDecl;
-typedef struct LilyCheckedScopeModule LilyCheckedScopeModule;
 
 typedef struct LilyCheckedDeclModule
 {
     String *name; // String* (&)
     String *global_name;
-    Vec *constants;      // Vec<LilyCheckedDecl*>*?
-    Vec *enums;          // Vec<LilyCheckedDecl*>*?
-    Vec *records;        // Vec<LilyCheckedDecl*>*?
-    Vec *aliases;        // Vec<LilyCheckedDecl*>*?
-    Vec *errors;         // Vec<LilyCheckedDecl*>*?
-    Vec *enums_object;   // Vec<LilyCheckedDecl*>*?
-    Vec *modules;        // Vec<LilyCheckedDecl*>*?
-    Vec *records_object; // Vec<LilyCheckedDecl*>*?
-    Vec *classes;        // Vec<LilyCheckedDecl*>*?
-    Vec *traits;         // Vec<LilyCheckedDecl*>*?
-    Vec *funs;           // Vec<LilyCheckedDecl*>*?
-    LilyCheckedScopeModule *scope;
+    Usize scope_id;
     enum LilyVisibility visibility;
-    bool is_checked;
 } LilyCheckedDeclModule;
 
 /**
@@ -63,132 +47,32 @@ inline CONSTRUCTOR(LilyCheckedDeclModule,
                    LilyCheckedDeclModule,
                    String *name,
                    String *global_name,
-                   Vec *decls,
-                   LilyCheckedScopeModule *scope,
+                   Usize scope_id,
                    enum LilyVisibility visibility)
 {
     return (LilyCheckedDeclModule){ .name = name,
                                     .global_name = global_name,
-                                    .constants = NULL,
-                                    .enums = NULL,
-                                    .records = NULL,
-                                    .aliases = NULL,
-                                    .errors = NULL,
-                                    .enums_object = NULL,
-                                    .modules = NULL,
-                                    .records_object = NULL,
-                                    .classes = NULL,
-                                    .traits = NULL,
-                                    .funs = NULL,
-                                    .scope = scope,
-                                    .visibility = visibility,
-                                    .is_checked = false };
+                                    .scope_id = scope_id,
+                                    .visibility = visibility };
 }
 
 /**
  *
- * @brief Push a constant to the module.
- */
-void
-push_constant__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                     LilyCheckedDecl *constant);
-
-/**
- *
- * @brief Push an enum to the module.
- */
-void
-push_enum__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                 LilyCheckedDecl *enum_);
-
-/**
- *
- * @brief Push a record to the module.
- */
-void
-push_record__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                   LilyCheckedDecl *record);
-
-/**
- *
- * @brief Push an alias to the module.
- */
-void
-push_alias__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                  LilyCheckedDecl *alias);
-
-/**
- *
- * @brief Push an error to the module.
- */
-void
-push_error__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                  LilyCheckedDecl *error);
-
-/**
- *
- * @brief Push an enum object to the module.
- */
-void
-push_enum_object__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                        LilyCheckedDecl *enum_object);
-
-/**
- *
- * @brief Push a module to the module.
- */
-void
-push_module__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                   LilyCheckedDecl *module);
-
-/**
- *
- * @brief Push a record object to the module.
- */
-void
-push_record_object__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                          LilyCheckedDecl *record_object);
-
-/**
- *
- * @brief Push a class to the module.
- */
-void
-push_class__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                  LilyCheckedDecl *class);
-
-/**
- *
- * @brief Push a trait to the module.
- */
-void
-push_trait__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                  LilyCheckedDecl *trait);
-
-/**
- *
- * @brief Push a fun to the module.
- */
-void
-push_fun__LilyCheckedDeclModule(LilyCheckedDeclModule *self,
-                                LilyCheckedDecl *fun);
-
-/**
- *
- * @brief Convert LilyCheckedDeclModule in String.
+ * @brief Convert LilyAstDeclModule in String.
  * @note This function is only used to debug.
  */
 #ifdef ENV_DEBUG
 String *
-IMPL_FOR_DEBUG(to_string,
-               LilyCheckedDeclModule,
-               const LilyCheckedDeclModule *self);
+IMPL_FOR_DEBUG(to_string, LilyAstDeclModule, const LilyAstDeclModule *self);
 #endif
 
 /**
  *
  * @brief Free LilyCheckedDeclModule type.
  */
-DESTRUCTOR(LilyCheckedDeclModule, const LilyCheckedDeclModule *self);
+inline DESTRUCTOR(LilyCheckedDeclModule, const LilyCheckedDeclModule *self)
+{
+    FREE(String, self->global_name);
+}
 
 #endif // LILY_CORE_LILY_ANALYSIS_CHECKED_DECL_MODULE_H
