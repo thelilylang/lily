f4649b95a384118afbb3683adce84b41dbd78117
ArthurPV <arthurpontiervalois@gmail.com>
2023-12-10 00:54:35 -0500

    refactor(scripts): Improve the code.

--- scripts/compare_ci.sh
+++ scripts/compare_ci.sh
@@ -4,45 +4,29 @@
 # This script checks whether CI (C Improved) is fully compatible
 # with C features, by comparing itself with the Clang and GCC compilers.
 
-DIR_CI_C89="./tests/core/cc/compare/ci/c89"
-DIR_CI_C99="./tests/core/cc/compare/ci/c99"
-DIR_CI_C11="./tests/core/cc/compare/ci/c11"
-DIR_CI_C17="./tests/core/cc/compare/ci/c17"
-DIR_CI_C23="./tests/core/cc/compare/ci/c23"
+C89=0
+C99=1
+C11=2
+C17=3
+C23=4
 
-DIR_C89="./tests/core/cc/compare/c89"
-DIR_C99="./tests/core/cc/compare/c99"
-DIR_C11="./tests/core/cc/compare/c11"
-DIR_C17="./tests/core/cc/compare/c17"
-DIR_C23="./tests/core/cc/compare/c23"
+BASE_DIR_CI="./tests/core/cc/compare/ci"
+DIR_CI=("$BASE_DIR_CI/c89" "$BASE_DIR_CI/c99" "$BASE_DIR_CI/c11" "$BASE_DIR_CI/c17" "$BASE_DIR_CI/c23")
+
+BASE_DIR="./tests/core/cc/compare/"
+DIR=("$BASE_DIR/c89" "$BASE_DIR/c99" "$BASE_DIR/c11" "$BASE_DIR/c17" "$BASE_DIR/c23")
 
 CLANG="clang"
 GCC="gcc"
 CI="./bin/ci"
 
-N_FILES_C89=0
-N_FILES_C99=0
-N_FILES_C11=0
-N_FILES_C17=0
-N_FILES_C23=0
+# C89, C99, C11, C17, C23
+N_FILES=(0 0 0 0 0)
 
-CLANG_SUCCESS_C89=0
-CLANG_SUCCESS_C99=0
-CLANG_SUCCESS_C11=0
-CLANG_SUCCESS_C17=0
-CLANG_SUCCESS_C23=0
-
-GCC_SUCCESS_C89=0
-GCC_SUCCESS_C99=0
-GCC_SUCCESS_C11=0
-GCC_SUCCESS_C17=0
-GCC_SUCCESS_C23=0
-
-CI_SUCCESS_C89=0
-CI_SUCCESS_C99=0
-CI_SUCCESS_C11=0
-CI_SUCCESS_C17=0
-CI_SUCCESS_C23=0
+# C89, C99, C11, C17, C23
+CLANG_SUCCESS=(0 0 0 0 0)
+GCC_SUCCESS=(0 0 0 0 0)
+CI_SUCCESS=(0 0 0 0 0)
 
 # $* options
 function clang_command() {
@@ -82,51 +66,50 @@ function check_status() {
 	fi
 }
 
-# $1 nb_standard_version
-# $2 standard_version_arg
+# $1 standard_version_arg
+# $2 C*
 function process_standard() {
-	local dir_c="DIR_C$1"
-	local dir_ci="DIR_CI_C$1"
+	local dir_c="DIR[$2]"
+	local dir_ci="DIR_CI[$2]"
 
 	while IFS= read -r -d '' FILE
 	do
-		check_status "clang_command -fsyntax-only -std=$2 $FILE" "inc_success CLANG_SUCCESS_C$1" $FILE $CLANG
-		check_status "gcc_command -fsyntax-only -std=$2 $FILE" "inc_success GCC_SUCCESS_C$1" $FILE $GCC
+		check_status "clang_command -fsyntax-only -std=$1 $FILE" "inc_success CLANG_SUCCESS[$2]" $FILE $CLANG
+		check_status "gcc_command -fsyntax-only -std=$1 $FILE" "inc_success GCC_SUCCESS[$2]" $FILE $GCC
 
-		let "N_FILES_C$1+=1"
+		let "N_FILES[$2]+=1"
 	done < <(find "${!dir_c}" -type f -print0)
 
 	while IFS= read -r -d '' FILE
 	do
-		check_status "ci_command --syntax -std=c$1 $FILE" "inc_success CI_SUCCESS_C$1" $FILE $CI
+		check_status "ci_command --syntax -std=c$1 $FILE" "inc_success CI_SUCCESS[$2]" $FILE $CI
 	done < <(find "${!dir_ci}" -type l -print0)
 }
 
-process_standard "89" "c89"
-process_standard "99" "c99"
-process_standard "11" "c11"
-process_standard "17" "c17"
-process_standard "23" "c2x"
+process_standard "c89" $C89
+process_standard "c99" $C99
+process_standard "c11" $C11
+process_standard "c17" $C17
+process_standard "c2x" $C23
+
+# $1 program
+function summary_program() {
+	local success_c89="$1_SUCCESS[$C89]"
+	local success_c99="$1_SUCCESS[$C99]"
+	local success_c11="$1_SUCCESS[$C11]"
+	local success_c17="$1_SUCCESS[$C17]"
+	local success_c23="$1_SUCCESS[$C23]"
+
+	echo "$1:"
+	echo "C89: ${!success_c89}/${N_FILES[$C89]} = $((${!success_c89}/${N_FILES[$C89]}))"
+	echo "C99: ${!success_c99}/${N_FILES[$C99]} = $((${!success_c99}/${N_FILES[$C99]}))"
+	echo "C11: ${!success_c11}/${N_FILES[$C11]} = $((${!success_c11}/${N_FILES[$C11]}))"
+	echo "C17: ${!success_c17}/${N_FILES[$C17]} = $((${!success_c17}/${N_FILES[$C17]}))"
+	echo "C23: ${!success_c23}/${N_FILES[$C23]} = $((${!success_c23}/${N_FILES[$C23]}))"
+	echo "Total: $(($(($((${!success_c89} + ${!success_c99} + ${!success_c11} + ${!success_c17} + ${!success_c23})) / $((${N_FILES[$C89]} + ${N_FILES[$C99]} + ${N_FILES[$C11]} + ${N_FILES[$C17]} + ${N_FILES[$C23]})))) * 100))%"
+}
 
 echo "Summary:"
-echo "Clang:"
-echo "C89: $CLANG_SUCCESS_C89/$N_FILES_C89 = $(($CLANG_SUCCESS_C89/$N_FILES_C89))"
-echo "C99: $CLANG_SUCCESS_C99/$N_FILES_C99 = $(($CLANG_SUCCESS_C99/$N_FILES_C99))"
-echo "C11: $CLANG_SUCCESS_C11/$N_FILES_C11 = $(($CLANG_SUCCESS_C11/$N_FILES_C11))"
-echo "C17: $CLANG_SUCCESS_C17/$N_FILES_C17 = $(($CLANG_SUCCESS_C17/$N_FILES_C17))"
-echo "C23: $CLANG_SUCCESS_C23/$N_FILES_C23 = $(($CLANG_SUCCESS_C23/$N_FILES_C23))"
-echo "Total: $(($(($(($CLANG_SUCCESS_C89 + $CLANG_SUCCESS_C99 + $CLANG_SUCCESS_C11 + $CLANG_SUCCESS_C17 + $CLANG_SUCCESS_C23)) / $(($N_FILES_C89 + $N_FILES_C99 + $N_FILES_C11 + $N_FILES_C17 + $N_FILES_C23)))) * 100))%"
-echo "GCC:"
-echo "C89: $GCC_SUCCESS_C89/$N_FILES_C89 = $(($GCC_SUCCESS_C89/$N_FILES_C89))"
-echo "C99: $GCC_SUCCESS_C99/$N_FILES_C99 = $(($GCC_SUCCESS_C99/$N_FILES_C99))"
-echo "C11: $GCC_SUCCESS_C11/$N_FILES_C11 = $(($GCC_SUCCESS_C11/$N_FILES_C11))"
-echo "C17: $GCC_SUCCESS_C17/$N_FILES_C17 = $(($GCC_SUCCESS_C17/$N_FILES_C17))"
-echo "C23: $GCC_SUCCESS_C23/$N_FILES_C23 = $(($GCC_SUCCESS_C23/$N_FILES_C23))"
-echo "Total: $(($(($(($GCC_SUCCESS_C89 + $GCC_SUCCESS_C99 + $GCC_SUCCESS_C11 + $GCC_SUCCESS_C17 + $GCC_SUCCESS_C23)) / $(($N_FILES_C89 + $N_FILES_C99 + $N_FILES_C11 + $N_FILES_C17 + $N_FILES_C23)))) * 100))%"
-echo "CI:"
-echo "C89: $CI_SUCCESS_C89/$N_FILES_C89 = $(($CI_SUCCESS_C89/$N_FILES_C89))"
-echo "C99: $CI_SUCCESS_C99/$N_FILES_C99 = $(($CI_SUCCESS_C99/$N_FILES_C99))"
-echo "C11: $CI_SUCCESS_C11/$N_FILES_C11 = $(($CI_SUCCESS_C11/$N_FILES_C11))"
-echo "C17: $CI_SUCCESS_C17/$N_FILES_C17 = $(($CI_SUCCESS_C17/$N_FILES_C17))"
-echo "C23: $CI_SUCCESS_C23/$N_FILES_C23 = $(($CI_SUCCESS_C23/$N_FILES_C23))"
-echo "Total: $(($(($(($CI_SUCCESS_C89 + $CI_SUCCESS_C99 + $CI_SUCCESS_C11 + $CI_SUCCESS_C17 + $CI_SUCCESS_C23)) / $(($N_FILES_C89 + $N_FILES_C99 + $N_FILES_C11 + $N_FILES_C17 + $N_FILES_C23)))) * 100))%"
+summary_program "CLANG"
+summary_program "GCC"
+summary_program "CI"
