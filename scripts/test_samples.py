925384ef6c57877ce20736c54de0589ec955cb9e
ArthurPV <arthurpontiervalois@gmail.com>
2024-02-29 18:30:13 -0500

    ci(scripts): Update `test_samples.py`.

--- scripts/test_samples.py
+++ scripts/test_samples.py
@@ -15,7 +15,7 @@ class OptionConfig:
 FILES = [
     y for x in os.walk("./tests/samples") for y in glob(os.path.join(x[0], "*.lily"))
 ]
-COMMAND = "./bin/lilyc"
+COMMAND = "./bin/Debug/lilyc"
 FILES_EXCLUDED = ["./tests/samples/package/a.lily", "./tests/samples/package/b.lily"]
 
 
