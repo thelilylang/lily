ca1a1d6845ae58ffd84a4e6da2196598907e2b82
ArthurPV <arthurpontiervalois@gmail.com>
2024-05-21 04:47:48 -0400

    docs(global): Update version to `0.1.0`.

--- include/cli/version.h
+++ include/cli/version.h
@@ -26,9 +26,9 @@
 #define LILY_CLI_VERSION_H
 
 #define MAJOR 0
-#define MINOR 0
-#define PATCH 2
+#define MINOR 1
+#define PATCH 0
 
-#define VERSION "0.0.2"
+#define VERSION "0.1.0"
 
 #endif // LILY_CLI_VERSION_H
