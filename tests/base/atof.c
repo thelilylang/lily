b491bdf94734daf2c6ceab1ba627faab2c375759
ArthurPV <arthurpontiervalois@gmail.com>
2024-10-06 16:13:53 -0400

    test(base): Improve tests of `atof__*` function.

--- tests/base/atof.c
+++ tests/base/atof.c
@@ -9,6 +9,11 @@ CASE(atof__Float32, {
     TEST_ASSERT(atof__Float32("2.2") == (float)2.2);
     TEST_ASSERT(atof__Float32("0.33") == (float)0.33);
     TEST_ASSERT(atof__Float32("111.222") == (float)111.222);
+    TEST_ASSERT(atof__Float32("2e+2") == (float)200.0);
+    TEST_ASSERT(atof__Float32("2e+5") == (float)200000.0);
+    TEST_ASSERT(atof__Float32("2e-2") == (float)0.02);
+    TEST_ASSERT(atof__Float32("2e-4") == (float)0.0002);
+    TEST_ASSERT(atof__Float32("2") == (float)2.0);
 });
 
 CASE(atof__Float64, {
@@ -16,4 +21,9 @@ CASE(atof__Float64, {
     TEST_ASSERT(atof__Float64("2.2") == 2.2);
     TEST_ASSERT(atof__Float64("0.33") == 0.33);
     TEST_ASSERT(atof__Float64("111.222") == 111.222);
+    TEST_ASSERT(atof__Float64("2e+1") == 20.0);
+    TEST_ASSERT(atof__Float64("2e+0") == 2.0);
+    TEST_ASSERT(atof__Float64("2e-2") == 0.02);
+    TEST_ASSERT(atof__Float64("2e-4") == 0.0002);
+    TEST_ASSERT(atof__Float64("3") == 3.0);
 });
