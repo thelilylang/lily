14f15bb281aa442df912da90810bee8899e8c389
ArthurPV <arthurpontiervalois@gmail.com>
2024-10-19 04:18:28 -0400

    ci(scripts): Add `scripts/compare_ci.sh` to full test script.

--- scripts/full_test.sh
+++ scripts/full_test.sh
@@ -17,7 +17,17 @@ function run_test {
 	echo
 }
 
+function print_header {
+	printf '=%.0s' {1..50}
+	printf "$1"
+	printf '=%.0s' {1..50}
+	printf '\n'
+	sleep 1
+}
+
 cmake --build build/Debug > /dev/null
+
+print_header "Lily"
 
 run_test ./bin/Debug/test_base
 run_test ./bin/Debug/test_core_scanner
@@ -26,5 +36,9 @@ run_test ./bin/Debug/test_core_precompiler
 run_test ./bin/Debug/test_core_parser
 run_test python ./scripts/test_samples.py
 run_test ./scripts/run_bins.sh
+
+print_header "CI"
+
+run_test ./scripts/compare_ci.sh
 
 exit $STATUS_CODE
