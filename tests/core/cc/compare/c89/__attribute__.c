a883c6cd1e4b235d6d1c40a6814f0ef515a31e54
ArthurPV <arthurpontiervalois@gmail.com>
2024-10-24 16:46:20 -0400

    fix(cc/ci): Fix test of `__attribute__`.

--- tests/core/cc/compare/c89/__attribute__.c
+++ tests/core/cc/compare/c89/__attribute__.c
@@ -2,7 +2,7 @@ void f() __attribute__((x));
 
 #define F2() void f2() __attribute__((x2))
 
-// From GNU C stdio.h
+/* From GNU C stdio.h */
 extern int snprintf (char *__restrict __s, unsigned long int __maxlen,
              const char *__restrict __format, ...)
      __attribute__ ((__format__ (__printf__, 3, 4)));
