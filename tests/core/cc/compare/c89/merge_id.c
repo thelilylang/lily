dbcff1205cb936b6aa3b0220074cab25a926bb09
ArthurPV <arthurpontiervalois@gmail.com>
2024-11-17 18:13:43 -0500

    test(cc/ci): Improve test of merging of ID.

--- tests/core/cc/compare/c89/merge_id.c
+++ tests/core/cc/compare/c89/merge_id.c
@@ -1,9 +1,13 @@
 #define MERGE(a, b) a##b
+#define J a
+#define MERGE_WITH_J(a) a##J
 
 int main() {
 	int xy = 10;
 	int a = MERGE(x, y);
 	int b = a + MERGE(1, 0);
+	int MERGE_WITH_J(x) = 30;
+	int *xJp = &xJ;
 
 	return 0;
 }
--- tests/core/cc/compare/c89/merge_keyword.c
+++ tests/core/cc/compare/c89/merge_keyword.c
@@ -1,7 +1,9 @@
 #define MERGE(a, b) a##b
 #define MERGE2(a, b, c) a##b##c
+#define MERGE3(a, b, c, d) a##b##c##d
 
 int main() {
 	int MERGE(if, 0) = 20;
 	MERGE2(l, o, ng) xx = 40;
+	MERGE3(l, o, n, g) abc = 100;
 }
