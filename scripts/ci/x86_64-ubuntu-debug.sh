450ae841a357d207455bfb51f8e7fbd70083d1d7
ArthurPV <arthurpontiervalois@gmail.com>
2025-01-11 18:11:56 -0500

    ci(scripts): Fix LLVM 16 mirrors are not not found.

--- scripts/ci/x86_64-ubuntu-debug.sh
+++ scripts/ci/x86_64-ubuntu-debug.sh
@@ -3,12 +3,17 @@
 set -e
 set -x
 
-# Download & Install LLVM
+LLVM_DIR='clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04'
+LLVM_ARCHIVE="$LLVM_DIR.tar.xz"
+PWD="$(pwd)"
+
+# Download & Uncompress LLVM
+wget -q "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/$LLVM_ARCHIVE"
+tar -xf "$LLVM_ARCHIVE"
+
+# Download & Install a C compiler
 sudo apt-get update
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 16 all
-sudo apt-get install liblld-16-dev
+sudo apt-get install clang libcurl4-openssl-dev
 
 # Download & Install Ninja
 sudo apt-get install ninja-build
@@ -26,10 +31,12 @@ make submodules_without_llvm
 cmake \
 	-B ./build/Debug \
 	-G Ninja \
+	-DLLVM_DIR="$PWD/$LLVM_DIR/lib/cmake/llvm" \
+	-DLLD_DIR="$PWD/$LLVM_DIR/lib/cmake/lld" \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DLILY_DEBUG=ON \
-	-DCMAKE_C_COMPILER=clang-16 \
-	-DCMAKE_CXX_COMPILER=clang++-16
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++
 
 # Build
 cmake --build build/Debug
