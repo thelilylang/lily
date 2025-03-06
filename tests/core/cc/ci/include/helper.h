c91c8954bb01826c49214e1563bd2531e635f1a6
ArthurPV <arthurpontiervalois@gmail.com>
2025-03-06 16:45:23 -0500

    test(cc/ci): Add new tests.

---
+++ tests/core/cc/ci/include/helper.h
@@ -0,0 +1,16 @@
+#ifndef LILY_TESTS_CORE_CC_CI_HELPER_H
+#define LILY_TESTS_CORE_CC_CI_HELPER_H
+
+#ifndef NULL
+#define NULL (void*)0
+#endif /* NULL */
+
+typedef __SIZE_TYPE__ size_t;
+
+extern int puts(const char *s);
+extern int printf(const char *s, ...);
+
+extern void *malloc(size_t size);
+extern void free(void *ptr); 
+
+#endif // LILY_TESTS_CORE_CC_CI_HELPER_H
