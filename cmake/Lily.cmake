b39822801615b5597dd90d0b3ad59443934ca95d
ArthurPV <arthurpontiervalois@gmail.com>
2025-03-11 13:28:27 -0400

    build(global): Improve CMake organization.

--- cmake/Lily.cmake
+++ cmake/Lily.cmake
@@ -1,2 +1,10 @@
-set(LILY_INCLUDE ${CMAKE_SOURCE_DIR}/include
-                 ${CMAKE_SOURCE_DIR}/lib/local/include ${CMAKE_SOURCE_DIR}/lib)
+set(LILY_INCLUDE
+    ${CMAKE_SOURCE_DIR}/include
+    ${CMAKE_SOURCE_DIR}/lib/local/include
+    ${CMAKE_SOURCE_DIR}/lib
+    ${CMAKE_SOURCE_DIR}/lib/local/src/llvm-project/llvm/include
+    ${CMAKE_SOURCE_DIR}/lib/local/src/llvm-project/lld/include)
+
+if(LILY_BUILD_LLVM)
+  set(LILY_INCLUDE ${LILY_INCLUDE} ${LILY_LLVM_BUILD_DIR}/include)
+endif()
